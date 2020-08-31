d6346b232485020a5a12cd93ef0359820d434fe0
chengkangzai <pycck@hotmail.com>
2020-08-31 22:36:54 +0800

    ?

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = 'J9z3k71ZwGhNfEj1fb8SGctbzClYmYbtay5gbeUeJbY='; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'SHGqNfkBLvvAcFmsmAoboRx7u5zFFgXnXsnanT3rR0g='; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
