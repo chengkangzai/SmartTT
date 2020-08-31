90197aa652bb3c788d9efecdc033fa5c1231fc3c
chengkangzai <pycck@hotmail.com>
2020-08-31 22:32:45 +0800

    addphpMyAdmin in route

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/test',function (){
     return DB::table('users')->get();
+});
+
+Route::get('/phpMyAdmin', function () {
+    return redirect('/phpMyAdmin/index.php');
 });
