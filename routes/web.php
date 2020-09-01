a76e47d5b876b7be2a318adc8c9f7d910bca7952
chengkangzai <pycck@hotmail.com>
2020-09-01 15:24:58 +0800

    test laravel subdomain

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::middleware('web')->domain(env('SITE_URL'))->group(function (){
         return view('index');
     });
 });
+
+Route::middleware('web')->domain('laravel' . env('SITE_URL'))->group(function (){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
