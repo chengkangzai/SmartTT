a5ab92c6837291dffedb96d3e707afa436d33dbf
chengkangzai <pycck@hotmail.com>
2020-09-01 15:13:24 +0800

    routing test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+
+Route::middleware('web')->domain(env('SITE_URL'))->group(function (){
+    Route::get('/', function () {
+        return view('index');
+    });
 });
