6f85c4b8ecb6c5d84a1bed801a3649524d1a52fc
chengkangzai <pycck@hotmail.com>
2020-09-05 11:15:55 +0800

    add web icon

--- resources/views/smartTT/layout/master.blade.php
+++ resources/views/smartTT/layout/master.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="/bower_components/Ionicons/css/ionicons.min.css">
     <link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
     <link rel="stylesheet" href="/dist/css/skins/skin-green.min.css">
+    <link rel='icon' href='icon.gif' type='image/gif' sizes='16x16'>
     @yield('style')
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
