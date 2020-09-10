5a9571d8ae7226bb913f71744de12ee28d40f655
chengkangzai <pycck@hotmail.com>
2020-09-10 09:46:46 +0800

    separate resource group

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,27 @@ Route::middleware('web')->domain(env('APP_URL'))->group(function () {
 });
 
 Route::middleware('web')->domain('smartTT.' . env('APP_URL'))->group(function () {
+    //TODO
+    //Public available page ..
     Route::get('/', 'DashboardController@index');
     Auth::routes();
+    Route::post('select2/getUserWithoutTheRole', 'Select2Controller@getUserWithoutTheRole')->name('select2.role.getUser');
+
+//    Auth::loginUsingId('1');
     Route::get('/dashboard', 'DashboardController@index')->name('home');
-    Route::resources([
-        'user' => 'UserController',
-        'tour' => 'TourController',
-        'trip' => 'TripController',
-        'airline' => 'AirlineController',
-        'booking' => 'BookingController',
-        'role' => 'RoleController',
+
+    Route::resource('user', 'UserController');
+    Route::resource('tour', 'TourController');
+    Route::resource('trip', 'TripController');
+    Route::resource('airline', 'AirlineController');
+    Route::resource('booking', 'BookingController');
+    Route::resource('tourDescription', 'TourDescriptionController')->only([
+        'store', 'show', 'edit', 'update', 'destroy'
     ]);
+    Route::put('role/addUserToRole/{role}}', 'RoleController@attachUser')->name('role.attachUserToRole');
+    Route::delete('role/removeUserToRole/{role}}', 'RoleController@detachUser')->name('role.detachUserToRole');
+    Route::resource('role', 'RoleController');
+
+
 });
 
