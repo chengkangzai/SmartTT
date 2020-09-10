04c2c0bcd59db250c309d9850bad690dec1af747
chengkangzai <pycck@hotmail.com>
2020-09-10 09:34:18 +0800

    add airline relationship

--- app/Trip.php
+++ app/Trip.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Trip extends Model
 {
     protected $fillable = [
-        'departure_datetime', 'fee', 'tour_id', 'capacity'
+        'departure_datetime', 'fee', 'tour_id', 'airline_id', 'capacity'
     ];
 
     //
     private function tour()
     {
         return $this->belongsTo('App\Tour');
+    }
+
+    private function airline()
+    {
+        return $this->hasOne('App\Airline');
     }
 }
