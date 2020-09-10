076aeeb379b8469c695321ba3634dfd078bbc0ff
chengkangzai <pycck@hotmail.com>
2020-09-10 09:37:51 +0800

    fix relationship ,mark method and specify table name

--- app/TourDescription.php
+++ app/TourDescription.php
@@ -4,7 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static create(array $toArray)
+ */
 class TourDescription extends Model
 {
-    //
+    protected $table = 'tour_description';
+    protected $fillable = [
+        'place', 'description', 'tour_id'
+    ];
+
+    private function tour()
+    {
+        return $this->belongsTo('App\Tour');
+    }
 }
