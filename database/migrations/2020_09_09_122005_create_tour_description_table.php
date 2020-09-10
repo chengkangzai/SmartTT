0ec56a667e3df9f66adce376bd0622ecab365202
chengkangzai <pycck@hotmail.com>
2020-09-10 09:43:07 +0800

    create Tour Description table

---
+++ database/migrations/2020_09_09_122005_create_tour_description_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTourDescriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tour_description', function (Blueprint $table) {
+            $table->id();
+            $table->text('place');
+            $table->text('description');
+            $table->foreignId('tour_id')->references('id')->on('tours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tour_description');
+    }
+}
