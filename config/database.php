b8825c2cc304b4290f8685be1d564cbc368d2899
chengkangzai <pycck@hotmail.com>
2020-09-16 21:32:31 +0800

    Add spatie backup and its configuration //ENV dependent

--- config/database.php
+++ config/database.php
@@ -61,6 +61,12 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'dump' => [
+//                'dump_binary_path' => '/bin/mysql', // only the path, so without `mysqldump` or `pg_dump`
+                'dump_binary_path' => 'C:/xampp/mysql/bin/', // only the path, so without `mysqldump` or `pg_dump`
+                'use_single_transaction',
+                'timeout' => 60 * 5, // 5 minute timeout
+            ]
         ],
 
         'pgsql' => [
@@ -123,7 +129,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
