eda682cd05eb828bc19c277d6083273438680d9f
chengkangzai <pycck@hotmail.com>
2021-02-20 23:34:41 +0800

    add airport and flight property to  factory
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- database/factories/FlightFactory.php
+++ database/factories/FlightFactory.php
@@ -3,9 +3,11 @@
 namespace Database\Factories;
 
 use App\Models\Airline;
+use App\Models\Airport;
 use App\Models\Flight;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use function array_rand;
 use function rand;
 
 class FlightFactory extends Factory
@@ -15,21 +17,26 @@ class FlightFactory extends Factory
      *
      * @var string
      */
-    protected $model = Flight::class;
+    protected string $model = Flight::class;
 
     /**
      * Define the model's default state.
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         $airlineCount = Airline::count();
+        $airportCount = Airport::count();
         return [
-            'depart_time' => Carbon::now()->addDay(rand(1, 30))->addSeconds(0,100000),
-            'arrive_time' => Carbon::tomorrow()->addDay(rand(35, 50))->addSeconds(0,100000),
+            'depart_time' => Carbon::now()->addDays(rand(1, 30))->addSeconds(rand(0, 100000)),
+            'arrive_time' => Carbon::tomorrow()->addDays(rand(1, 30))->addSeconds(rand(0, 100000)),
             'fee' => rand(10000, 200000),
-            'airline_id' => rand(1, $airlineCount)
+            'airline_id' => rand(1, $airlineCount),
+            'depart_airport' => rand(1, $airportCount),
+            'arrival_airport' => rand(1, $airportCount),
+            'flight_class' => array_rand(Flight::FCLASS),
+            'flight_type' => array_rand(Flight::TYPE),
         ];
     }
 }
