bf4048b260e4ee34d9532353957e5a892b6a6d2c
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-15 00:29:13 +0800

    Convert User to Actions

--- resources/views/smartTT/trip/create.blade.php
+++ resources/views/smartTT/trip/create.blade.php
@@ -21,33 +21,44 @@
                 @include('partials.error-alert')
                 @csrf
                 <div class="mb-3">
-                    <label for="fee" class="form-label">Fee (RM)</label>
+                    <label for="fee" class="form-label">{{__('Fee (RM)')}}</label>
                     <input type="number" name="fee" class="form-control" id="fee" value="{{ old('fee') }}"
-                        placeholder="{{ __('Enter Trip Fee') }}">
+                           placeholder="{{ __('Enter Trip Fee') }}">
                 </div>
                 <div class="mb-3">
-                    <label for="capacity" class="form-label">Capacity</label>
-                    <input type="number" name="capacity" class="form-control" id="capacity" value="{{ old('capacity') }}"
-                        placeholder="{{ __('Enter Capacity of this trip') }}">
+                    <label for="capacity" class="form-label">{{__('Capacity')}}</label>
+                    <input type="number" name="capacity" class="form-control" id="capacity"
+                           value="{{ old('capacity') }}"
+                           placeholder="{{ __('Enter Capacity of this trip') }}">
                 </div>
 
                 <div class="mb-3">
-                    <label for="tour" class="form-label">Tour</label>
-                    <select id="tour" name="tour" class="form-control">
+                    <label for="tour_id" class="form-label">{{__('Tour')}}</label>
+                    <select id="tour_id" name="tour_id" class="form-control">
                         @foreach ($tours as $tour)
-                            <option value="{{ $tour->id }}"> {{ $tour->name }} </option>
+                            <option value="{{ $tour->id }}"@selected(old('tour_id') == $tour->id)>
+                                {{ $tour->name }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
 
                 <div class="mb-3">
-                    <label class="form-label" for="depart_time">Depart Time</label>
-                    <input type="datetime-local" class="form-control" name="depart_time" id="depart_time" />
+                    <label class="form-label" for="depart_time">{{__('Depart Time')}}</label>
+                    <input type="datetime-local" class="form-control" name="depart_time" id="depart_time"
+                           value="{{old('depart_time')}}"/>
                 </div>
 
                 <div class="mb-3">
-                    <label class="form-label" for="flightSelect">Flight</label>
-                    <select name="flight[]" class="form-control select2 " id="flightSelect" multiple></select>
+                    <label class="form-label" for="flights">{{__('Flight')}}</label>
+                    <select name="flights[]" class="form-control select2" id="flights" multiple>
+                        <option value="">{{__('Select Flight')}}</option>
+                        @foreach ($flights as $flight)
+                            <option value="{{ $flight->id }}" @selected(old('flights') == $flight->id)>
+                                {{ $flight->text }}
+                            </option>
+                        @endforeach
+                    </select>
                 </div>
             </form>
         </div>
@@ -59,16 +70,8 @@
 
 @section('script')
     <script>
-        $.ajax({
-            type: "POST",
-            url: "{{ route('select2.trip.getFlight') }}",
-            success: function(response) {
-                console.log(response);
-                $("#flightSelect").empty();
-                $("#flightSelect").select2({
-                    data: response
-                });
-            }
+        $("#flights").select2({
+            placeholder: "{{__('Select Flight')}}",
         });
     </script>
 @endsection
