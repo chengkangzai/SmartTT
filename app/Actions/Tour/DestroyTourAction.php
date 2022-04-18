f2419ac70687ca3ad1d51f0a8ebebe4dbac195df
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-18 22:40:18 +0800

    remove comment close https://github.com/chengkangzai/SmartTT/pull/22#discussion_r852147167

--- app/Actions/Tour/DestroyTourAction.php
+++ app/Actions/Tour/DestroyTourAction.php
@@ -12,7 +12,6 @@ class DestroyTourAction
      */
     public function execute(Tour $tour): void
     {
-        //make sure the tour do not have package
         if ($tour->packages->count() > 0) {
             throw  new \Exception('This tour has package, you can not delete it, please delete the package first');
         }
