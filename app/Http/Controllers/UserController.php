c93fce7a8975bdd8616206db339bb4740ea235f2
chengkangzai <chengkangzai@users.noreply.github.com>
2022-04-21 13:37:32 +0000

    Fix styling

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,6 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Password;
 
 class UserController extends Controller
 {
