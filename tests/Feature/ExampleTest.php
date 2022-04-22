9bf99409febae15a65a2411ac7d40b3ef9f94c01
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-22 13:17:41 +0800

    initialize pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,18 +1,4 @@
 <?php
-
-namespace Tests\Feature;
-
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $this->assertTrue(true);
-    }
-}
+ it('can run an example test', function () {
+    $this->assertTrue(true);
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,4 @@
 <?php
-
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
-    {
-        $this->assertTrue(true);
-    }
-}
+it('should be true', function () {
+    $this->assertTrue(true);
+});
