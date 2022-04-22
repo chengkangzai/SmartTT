efb0eefa5fff8eb0cfa0d612a3d29f01cbb77769
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-22 21:47:48 +0800

    fix factory not working

--- database/factories/FlightFactory.php
+++ database/factories/FlightFactory.php
@@ -18,15 +18,14 @@ class FlightFactory extends Factory
     #[ArrayShape(['departure_date' => "\Illuminate\Support\Carbon", 'arrival_date' => "\Illuminate\Support\Carbon", 'price' => "int", 'airline_id' => "int", 'departure_airport_id' => "int", 'arrival_airport_id' => "int", 'class' => "array|int|string", 'type' => "array|int|string"])]
     public function definition(): array
     {
-        $airlineCount = Airline::count();
-        $airportCount = Airport::count();
+        $airport= Airport::inRandomOrder()->take(2)->get();
         return [
             'departure_date' => now()->addDays(rand(1, 30))->addSeconds(rand(0, 100000)),
             'arrival_date' => now()->addDays(rand(1, 30))->addSeconds(rand(0, 100000)),
             'price' => rand(100, 2000),
-            'airline_id' => rand(1, $airlineCount),
-            'departure_airport_id' => rand(1, $airportCount),
-            'arrival_airport_id' => rand(1, $airportCount),
+            'airline_id' => Airline::inRandomOrder()->first()->id,
+            'departure_airport_id' => $airport[0]->id,
+            'arrival_airport_id' => $airport[1]->id,
             'class' => array_rand(Flight::FCLASS),
             'type' => array_rand(Flight::TYPE),
         ];
