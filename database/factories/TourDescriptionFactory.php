b04355391f7828b3729791c702bead4c3273b9f2
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-23 23:16:07 +0800

    fix TourDescriptionFactory

--- database/factories/TourDescriptionFactory.php
+++ database/factories/TourDescriptionFactory.php
@@ -7,20 +7,20 @@ use App\Models\TourDescription;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use JetBrains\PhpStorm\ArrayShape;
 use function join;
-use function rand;
 
 class TourDescriptionFactory extends Factory
 {
     protected $model = TourDescription::class;
 
-    #[ArrayShape(['place' => "string", 'description' => "string", 'tour_id' => "int"])]
+    #[ArrayShape(['place' => "string", 'description' => "string", 'tour_id' => "int", 'order' => "int"])]
     public function definition(): array
     {
-        $maxNumber = Tour::count();
+        $tour = Tour::inRandomOrder()->first();
         return [
             'place' => join(" ", $this->faker->words(3)),
             'description' => $this->faker->text,
-            'tour_id' => rand(1, $maxNumber)
+            'tour_id' => $tour->id,
+            'order' => TourDescription::whereBelongsTo($tour)->count() + 1,
         ];
     }
 }
