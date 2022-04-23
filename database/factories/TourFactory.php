bc306beca31115c09827ba647b399688b092968e
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-23 17:35:31 +0800

    put itinerary_url and thumbnail_url default as empty string

--- database/factories/TourFactory.php
+++ database/factories/TourFactory.php
@@ -23,10 +23,21 @@ class TourFactory extends Factory
             'tour_code' => rand(1, 5) . strtoupper($this->faker->randomLetter) . strtoupper($this->faker->randomLetter) . strtoupper($this->faker->randomLetter),
             'name' => rand(1, 5) . "D" . rand(1, 5) . "N " . $country->name . " Package",
             'category' => $selection[rand(0, 4)],
-            'itinerary_url' => Storage::putFile('public/Tour/itinerary', $this->faker->image(), 'public'),
-            'thumbnail_url' => Storage::putFile('public/Tour/thumbnail', $this->faker->image(), 'public'),
+            'itinerary_url' => '',
+            'thumbnail_url' => '',
             'nights' => rand(1, 5),
             'days' => rand(1, 5),
         ];
     }
+
+    public function withFakerItineraryAndThumbnail(): TourFactory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'itinerary_url' => Storage::putFile('public/Tour/itinerary', $this->faker->image(), 'public'),
+                'thumbnail_url' => Storage::putFile('public/Tour/thumbnail', $this->faker->image(), 'public'),
+            ];
+        });
+    }
+
 }
