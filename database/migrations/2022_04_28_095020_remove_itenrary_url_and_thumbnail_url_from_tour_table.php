e1c1e17bb35bf84d3e9216747f4103edf64edca9
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-28 09:51:34 +0800

    remove thumbnail_url and itinerary_url from tour

---
+++ database/migrations/2022_04_28_095020_remove_itenrary_url_and_thumbnail_url_from_tour_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('tours', function (Blueprint $table) {
+            $table->dropColumn('itinerary_url');
+            $table->dropColumn('thumbnail_url');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('tours', function (Blueprint $table) {
+            $table->string('itinerary_url')->nullable();
+            $table->string('thumbnail_url')->nullable();
+        });
+    }
+};
