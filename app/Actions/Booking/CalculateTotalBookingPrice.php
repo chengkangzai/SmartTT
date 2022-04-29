a88847b5d35422cbbad9f54a46600c6795ba970c
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-30 03:20:16 +0800

    fix bug

--- app/Actions/Booking/CalculateTotalBookingPrice.php
+++ app/Actions/Booking/CalculateTotalBookingPrice.php
@@ -7,10 +7,14 @@ use App\Models\Settings\BookingSetting;
 
 trait CalculateTotalBookingPrice
 {
-    public function calculate($data, BookingSetting $bookingSetting): float|int
+    public function __construct(
+        private readonly BookingSetting $bookingSetting,
+    ){}
+
+    public function calculate($data): float|int
     {
         $packagePrice = Package::findOrFail($data['package_id'])->price;
 
-        return $packagePrice * $data['adult'] + ($bookingSetting->charge_per_child * $data['child']) - $data['discount'];
+        return $packagePrice * $data['adult'] + ($this->bookingSetting->charge_per_child * $data['child']) - $data['discount'];
     }
 }
