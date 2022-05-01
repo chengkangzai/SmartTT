4b2c2da6d012412a2c103c1a5b10f3a7e602c997
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-01 19:38:21 +0800

    add Action Test

---
+++ tests/Feature/Actions/Setting/Edit/GetViewBagForTourSettingActionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Actions\Setting\Edit\GetViewBagForTourSettingAction;
+use Database\Seeders\CountrySeeder;
+use Database\Seeders\TourSeeder;
+use Illuminate\Support\Collection;
+use function Pest\Laravel\seed;
+
+beforeEach(function () {
+    seed(CountrySeeder::class);
+    seed(TourSeeder::class);
+});
+
+it('returns the view bag for the tour setting', function () {
+    $action = app(GetViewBagForTourSettingAction::class);
+    $viewBag = $action->execute();
+
+    expect($viewBag)->toBeArray();
+    expect($viewBag['tours'])->toBeArray();
+});
