1a150d610690dba85914227fe4b38a775b0ee2c4
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-04 13:26:59 +0800

    fix money is not set up properly

--- app/Models/PackagePricing.php
+++ app/Models/PackagePricing.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -24,6 +25,14 @@ class PackagePricing extends Model
         'available_capacity',
         'is_active',
     ];
+
+    public function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
+    }
 
     public function package(): BelongsTo
     {
