9f40e11be19be6b5701c0e0913ef98b3fabef343
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-04 14:36:07 +0800

    format code

--- resources/views/livewire/booking/create-booking-card.blade.php
+++ resources/views/livewire/booking/create-booking-card.blade.php
@@ -1,6 +1,6 @@
 @php
-    /** @var \App\Models\Package $package */
-    /** @var \App\Models\PackagePricing $pricing */
+/** @var \App\Models\Package $package */
+/** @var \App\Models\PackagePricing $pricing */
 @endphp
 
 <div class="card">
@@ -35,7 +35,7 @@
                         <td>{{ $package->pricings->sum('available_capacity') }}</td>
                         <td>
                             <input type="radio" wire:model="package" value="{{ $package->id }}"
-                                   aria-label="{{ __('Choose this package') }}">
+                                aria-label="{{ __('Choose this package') }}">
                         </td>
                     </tr>
                 @empty
@@ -53,106 +53,114 @@
             </div>
             <table class="table table-striped table-bordered">
                 <thead>
-                <tr>
-                    <th>{{ __('No') }}</th>
-                    <th>{{ __('Guest Name') }}</th>
-                    <th>{{ __('Package') }} ({{ $defaultCurrency }})</th>
-                    <th>{{ __('Price') }} ({{ $defaultCurrency }})</th>
-                    <th>{{ __('Action') }}</th>
-                </tr>
+                    <tr>
+                        <th>{{ __('No') }}</th>
+                        <th>{{ __('Guest Name') }}</th>
+                        <th>{{ __('Package') }} ({{ $defaultCurrency }})</th>
+                        <th>{{ __('Price') }} ({{ $defaultCurrency }})</th>
+                        <th>{{ __('Action') }}</th>
+                    </tr>
                 </thead>
                 <tbody>
-                @foreach ($guests as $i => $guest)
-                    <tr>
-                        <td>
-                            {{ $i + 1 }}
-                        </td>
-                        <td>
-                            <input type="text" wire:model="guests.{{ $i }}.name"
-                                   id="guest-{{ $i }}-name" class="form-control" aria-label="Name">
-                        </td>
-                        <td>
-                            @if (!$guest['is_child'])
-                                <select wire:model="guests.{{ $i }}.pricing"
+                    @foreach ($guests as $i => $guest)
+                        <tr>
+                            <td>
+                                {{ $i + 1 }}
+                            </td>
+                            <td>
+                                <input type="text" wire:model="guests.{{ $i }}.name"
+                                    id="guest-{{ $i }}-name" class="form-control" aria-label="Name">
+                            </td>
+                            <td>
+                                @if (!$guest['is_child'])
+                                    <select wire:model="guests.{{ $i }}.pricing"
                                         id="guest-{{ $i }}-pricings" class="form-control"
                                         aria-label="Pricing" required wire:change="updatePrice({{ $i }})">
-                                    @foreach ($pricingsHolder as $pricing)
-                                        <option value="{{ $pricing['id'] }}"
+                                        @foreach ($pricingsHolder as $pricing)
+                                            <option value="{{ $pricing['id'] }}"
                                                 @if ($loop->first) selected @endif>
-                                            {{ $pricing['name'] }}
-                                            ({{ $pricing['available_capacity'] . ' ' . __('Left') }})
-                                            ({{ number_format($pricing['price'], 2) }})
-                                        </option>
-                                    @endforeach
-                                </select>
-                            @else
-                                <p class="float-end"> {{ number_format($guest['price'], 2) }}</p>
-                            @endif
-                        </td>
-                        <td>
-                            <p> {{ number_format($guest['price'], 2) }}</p>
-                        </td>
-                        <td>
-                            <button wire:click="removeGuest({{ $i }})" class="btn btn-outline-danger">
-                                <svg class="icon icon-lg">
-                                    <use xlink:href="{{ asset('icons/coreui.svg#cil-trash') }}"></use>
-                                </svg>
-                            </button>
-                        </td>
-                    </tr>
-                @endforeach
+                                                {{ $pricing['name'] }}
+                                                ({{ $pricing['available_capacity'] . ' ' . __('Left') }})
+                                                ({{ number_format($pricing['price'], 2) }})
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                @else
+                                    <p class="float-end"> {{ number_format($guest['price'], 2) }}</p>
+                                @endif
+                            </td>
+                            <td>
+                                <p> {{ number_format($guest['price'], 2) }}</p>
+                            </td>
+                            <td>
+                                <button wire:click="removeGuest({{ $i }})" class="btn btn-outline-danger">
+                                    <svg class="icon icon-lg">
+                                        <use xlink:href="{{ asset('icons/coreui.svg#cil-trash') }}"></use>
+                                    </svg>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
                 <tfoot>
-                <tr>
-                    <td colspan="3">
-                        <p class="float-end fw-bold ">{{ __('Total Price') }}</p>
-                    </td>
-                    <td colspan="2">
-                        <p class="fw-bold">{{ number_format($totalPrice, 2) }}</p>
-                    </td>
+                    <tr>
+                        <td colspan="3">
+                            <p class="float-end fw-bold ">{{ __('Total Price') }}</p>
+                        </td>
+                        <td colspan="2">
+                            <p class="fw-bold">{{ number_format($totalPrice, 2) }}</p>
+                        </td>
                 </tfoot>
             </table>
 
         @endif
         @if ($currentStep == 4)
-            <p>{{__('Please confirm your booking details')}}</p>
+            <p>{{ __('Please confirm your booking details') }}</p>
             <table class="table table-bordered">
                 <tr>
-                    <td><p class="float-end">{{ __('Tour Name') }}</p></td>
+                    <td>
+                        <p class="float-end">{{ __('Tour Name') }}</p>
+                    </td>
                     <td>{{ $tourModel->name }}</td>
                 </tr>
                 <tr>
-                    <td><p class="float-end">{{ __('Itinerary') }}</p></td>
+                    <td>
+                        <p class="float-end">{{ __('Itinerary') }}</p>
+                    </td>
                     <td>
                         <a href="{{ $tourModel->getFirstMedia('itinerary')->getUrl() }}" target="_blank"
-                           class="btn btn-outline-primary">
+                            class="btn btn-outline-primary">
                             {{ __('Itinerary') }}
                         </a>
                     </td>
                 </tr>
                 <tr>
-                    <td><p class="float-end">{{ __('Depart Date') }}</p></td>
+                    <td>
+                        <p class="float-end">{{ __('Depart Date') }}</p>
+                    </td>
                     <td>{{ $packageModel->depart_time->toDayDateTimeString() }}</td>
                 </tr>
                 <tr>
-                    <td><p class="float-end">{{ __('Guests') }}</p></td>
+                    <td>
+                        <p class="float-end">{{ __('Guests') }}</p>
+                    </td>
                     <td>
                         <table class="table table-bordered">
                             <thead>
-                            <tr>
-                                <th>{{ __('Name') }}</th>
-                                <th>{{__('Package')}}</th>
-                                <th>{{__('Price')}} ({{$defaultCurrency}})</th>
-                            </tr>
+                                <tr>
+                                    <th>{{ __('Name') }}</th>
+                                    <th>{{ __('Package') }}</th>
+                                    <th>{{ __('Price') }} ({{ $defaultCurrency }})</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            @foreach($guests as $guest)
-                                <tr>
-                                    <td>{{ $guest['name'] }}</td>
-                                    <td>{{ $pricings->find($guest['pricing'])->name }}</td>
-                                    <td>{{ number_format($guest['price'], 2) }}</td>
-                                </tr>
-                            @endforeach
+                                @foreach ($guests as $guest)
+                                    <tr>
+                                        <td>{{ $guest['name'] }}</td>
+                                        <td>{{ $pricings->find($guest['pricing'])->name }}</td>
+                                        <td>{{ number_format($guest['price'], 2) }}</td>
+                                    </tr>
+                                @endforeach
                         </table>
                     </td>
                 </tr>
@@ -167,7 +175,7 @@
             @if ($currentStep < 4)
                 <button wire:click="nextStep" class="btn btn-primary mx-1">{{ __('Next') }} &rarr;</button>
             @endif
-            @if($currentStep == 4)
+            @if ($currentStep == 4)
                 <button wire:click="nextStep" class="btn btn-success mx-1">
                     {{ __('Confirm & Proceed to Payment') }} &rarr;
                 </button>
