9323979f5851e0e20b07a4601225c71254803b40
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-05 12:05:40 +0800

    make model logged

--- app/Models/BookingGuest.php
+++ app/Models/BookingGuest.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class BookingGuest extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use LogsActivity;
 
     public $fillable = [
         'name',
@@ -30,5 +33,10 @@ class BookingGuest extends Model
     public function packagePricing(): BelongsTo
     {
         return $this->belongsTo(PackagePricing::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logFillable()->logOnlyDirty();
     }
 }
