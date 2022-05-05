703807e636e677cd9233106fd4b479f47a00d3ee
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-05 15:18:43 +0800

    introduce Laravel Cashier

---
+++ database/migrations/2022_05_04_112312_create_payments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('booking_id')->constrained();
+            $table->string('payment_method')->nullable();
+            $table->string('status');
+            $table->integer('amount');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
