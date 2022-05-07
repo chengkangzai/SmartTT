7df24355cd53c132f491e48ebc266c6ac9c05dcc
chengkangzai <chengkangzai@users.noreply.github.com>
2022-05-07 05:51:13 +0000

    Fix styling

--- app/Http/Livewire/Booking/CreateBookingCard.php
+++ app/Http/Livewire/Booking/CreateBookingCard.php
@@ -20,172 +20,171 @@ use Illuminate\Support\Carbon;
 use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 use Stripe\SetupIntent;
- use function sleep;
 
  class CreateBookingCard extends Component
-{
-    #region Step 1
-    public Collection $tours;
-    public int $tour = 0;
-    #endregion
-    #region Step 2
-    public Collection $packages;
-    public int $package = 0;
-    #endregion
-    #region Step 3
-    public Collection $pricings;
-    public int $pricing = 0;
-    public array $pricingsHolder;
-    public int $totalPrice = 0;
-    #endregion
-    #region Step 4
-    public array $guests = [];
-    private Booking $booking;
-    public int $bookingId;
-    #endregion
-    #region Step 5
-    public string $paymentType = 'full';
-    public string $paymentMethod;
-    public string $manualType = 'card';
-    public bool $paymentCashReceived = false;
+ {
+     #region Step 1
+     public Collection $tours;
+     public int $tour = 0;
+     #endregion
+     #region Step 2
+     public Collection $packages;
+     public int $package = 0;
+     #endregion
+     #region Step 3
+     public Collection $pricings;
+     public int $pricing = 0;
+     public array $pricingsHolder;
+     public int $totalPrice = 0;
+     #endregion
+     #region Step 4
+     public array $guests = [];
+     private Booking $booking;
+     public int $bookingId;
+     #endregion
+     #region Step 5
+     public string $paymentType = 'full';
+     public string $paymentMethod;
+     public string $manualType = 'card';
+     public bool $paymentCashReceived = false;
 
-    public string $cardHolderName = '';
-    public string $cardNumber = '';
-    public string $cardExpiry = '';
-    public string $cardCvc = '';
+     public string $cardHolderName = '';
+     public string $cardNumber = '';
+     public string $cardExpiry = '';
+     public string $cardCvc = '';
 
-    private array $validateCardRule = [
+     private array $validateCardRule = [
         'cardHolderName' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z ]+$/'],
         'cardNumber' => ['required', 'string', 'max:255', 'regex:/^[0-9]{16}$/'],
         'cardExpiry' => ['required', 'string', 'max:255', 'regex:/^[0-9]{2}\/[0-9]{2}$/'], // MM/YY
         'cardCvc' => ['required', 'string', 'max:255', 'regex:/^[0-9]{3,4}$/'],
     ];
 
-    public string $stripePaymentMethod = '';
-    public int $paymentAmount = 0;
-    private SetupIntent $paymentIntent;
-    #endregion
+     public string $stripePaymentMethod = '';
+     public int $paymentAmount = 0;
+     private SetupIntent $paymentIntent;
+     #endregion
 
-    public int $currentStep = 1;
-    public string $defaultCurrency;
-    public int $charge_per_child;
+     public int $currentStep = 1;
+     public string $defaultCurrency;
+     public int $charge_per_child;
 
-    protected $listeners = ['cardSetupConfirmed'];
+     protected $listeners = ['cardSetupConfirmed'];
 
-    public function mount()
-    {
-        $this->tours = Tour::active()->get(['id', 'name']);
-        $this->defaultCurrency = app(GeneralSetting::class)->default_currency;
-        $this->charge_per_child = app(BookingSetting::class)->charge_per_child;
-        $this->paymentMethod = auth()->user()->hasRole('Customer') ? 'stripe' : 'manual';
-    }
+     public function mount()
+     {
+         $this->tours = Tour::active()->get(['id', 'name']);
+         $this->defaultCurrency = app(GeneralSetting::class)->default_currency;
+         $this->charge_per_child = app(BookingSetting::class)->charge_per_child;
+         $this->paymentMethod = auth()->user()->hasRole('Customer') ? 'stripe' : 'manual';
+     }
 
-    public function render(): Factory|View|Application
-    {
-        return view('livewire.booking.create-booking-card', [
+     public function render(): Factory|View|Application
+     {
+         return view('livewire.booking.create-booking-card', [
             'tours' => $this->tours,
         ]);
-    }
+     }
 
-    #region Step 1 - Select Tour
-    public function updatedTour()
-    {
-        $this->resetErrorBag();
-        if ($this->tour == 0) {
-            $this->addError('tour', __('Please select a tour'));
+     #region Step 1 - Select Tour
+     public function updatedTour()
+     {
+         $this->resetErrorBag();
+         if ($this->tour == 0) {
+             $this->addError('tour', __('Please select a tour'));
 
-            return;
-        }
-        $this->currentStep++;
-        $this->packages = Package::where('tour_id', $this->tour)->active()->get();
-    }
-    #endregion
+             return;
+         }
+         $this->currentStep++;
+         $this->packages = Package::where('tour_id', $this->tour)->active()->get();
+     }
+     #endregion
 
-    #region Step 2 - Select Package
-    public function updatedPackage()
-    {
-        $this->resetErrorBag();
-        if ($this->package == 0) {
-            $this->addError('package', __('Please select a package'));
+     #region Step 2 - Select Package
+     public function updatedPackage()
+     {
+         $this->resetErrorBag();
+         if ($this->package == 0) {
+             $this->addError('package', __('Please select a package'));
 
-            return;
-        }
-        $this->currentStep++;
-        $this->updatePricings();
-        if (count($this->guests) == 0) {
-            $this->addNewGuest();
-        } else {
-            $this->updatePrice(0);
-        }
-    }
+             return;
+         }
+         $this->currentStep++;
+         $this->updatePricings();
+         if (count($this->guests) == 0) {
+             $this->addNewGuest();
+         } else {
+             $this->updatePrice(0);
+         }
+     }
 
-    private function updatePricings(): void
-    {
-        $this->pricings = PackagePricing::query()
+     private function updatePricings(): void
+     {
+         $this->pricings = PackagePricing::query()
             ->where('package_id', $this->package)
             ->where('available_capacity', '>', 1)
             ->active()
             ->orderBy('price')
             ->get();
-        $this->pricingsHolder = $this->pricings->toArray();
-    }
-    #endregion
+         $this->pricingsHolder = $this->pricings->toArray();
+     }
+     #endregion
 
-    #region Step 3 - Select Pricing & Guests
-    public function addNewGuest()
-    {
-        $cheapestPricing = $this->pricings->first();
-        $this->guests[] = ['name' => '', 'pricing' => $cheapestPricing->id, 'price' => $cheapestPricing->price, 'is_child' => false];
-        $this->updatePrice(count($this->guests) - 1);
-    }
+     #region Step 3 - Select Pricing & Guests
+     public function addNewGuest()
+     {
+         $cheapestPricing = $this->pricings->first();
+         $this->guests[] = ['name' => '', 'pricing' => $cheapestPricing->id, 'price' => $cheapestPricing->price, 'is_child' => false];
+         $this->updatePrice(count($this->guests) - 1);
+     }
 
-    public function addNewChild()
-    {
-        $this->guests[] = ['name' => '', 'pricing' => 0, 'price' => $this->charge_per_child, 'is_child' => true];
-        $this->updatePrice(0);
-    }
+     public function addNewChild()
+     {
+         $this->guests[] = ['name' => '', 'pricing' => 0, 'price' => $this->charge_per_child, 'is_child' => true];
+         $this->updatePrice(0);
+     }
 
-    public function removeGuest($index)
-    {
-        unset($this->guests[$index]);
-        $this->guests = array_values($this->guests);
-        $this->updatePrice(count($this->guests) - 1);
-    }
+     public function removeGuest($index)
+     {
+         unset($this->guests[$index]);
+         $this->guests = array_values($this->guests);
+         $this->updatePrice(count($this->guests) - 1);
+     }
 
-    public function updatePrice($index)
-    {
-        if (! $this->guests[$index]['is_child']) {
-            $this->guests[$index]['price'] = $this->pricings->find($this->guests[$index]['pricing'])->price;
-        }
-        $this->totalPrice = collect($this->guests)->sum('price');
-    }
+     public function updatePrice($index)
+     {
+         if (! $this->guests[$index]['is_child']) {
+             $this->guests[$index]['price'] = $this->pricings->find($this->guests[$index]['pricing'])->price;
+         }
+         $this->totalPrice = collect($this->guests)->sum('price');
+     }
 
-    public function validateGuest()
-    {
-        $this->resetErrorBag();
+     public function validateGuest()
+     {
+         $this->resetErrorBag();
 
-        try {
-            app(ValidateBookingGuestAction::class)->execute($this->pricingsHolder, [
+         try {
+             app(ValidateBookingGuestAction::class)->execute($this->pricingsHolder, [
                 'guests' => $this->guests,
                 'tour' => $this->tour,
                 'package' => $this->package,
             ]);
-        } catch (ValidationException $e) {
-            $this->setErrorBag($e->validator->errors());
-            $this->updatePricings();
+         } catch (ValidationException $e) {
+             $this->setErrorBag($e->validator->errors());
+             $this->updatePricings();
 
-            return;
-        }
+             return;
+         }
 
-        $this->currentStep++;
-    }
-    #endregion
+         $this->currentStep++;
+     }
+     #endregion
 
-    #region Step 4 - Confirm Booking
-    public function saveBooking()
-    {
-        if (! isset($this->booking)) {
-            $booking = app(StoreBookingAction::class)->execute(auth()->user(), [
+     #region Step 4 - Confirm Booking
+     public function saveBooking()
+     {
+         if (! isset($this->booking)) {
+             $booking = app(StoreBookingAction::class)->execute(auth()->user(), [
                 'package_id' => $this->package,
                 'adult' => collect($this->guests)->where('is_child', false)->count(),
                 'child' => collect($this->guests)->where('is_child', true)->count(),
@@ -193,19 +192,19 @@ use Stripe\SetupIntent;
                 'guests' => $this->guests,
             ]);
 
-            $this->booking = $booking;
-            $this->bookingId = $booking->id;
-        }
+             $this->booking = $booking;
+             $this->bookingId = $booking->id;
+         }
 
-        $this->getReadyForPayment();
-        $this->currentStep++;
-    }
-    #endregion
+         $this->getReadyForPayment();
+         $this->currentStep++;
+     }
+     #endregion
 
-    #region Step 5 - Payment
-    public function getReadyForPayment()
-    {
-        $payment = Payment::updateOrCreate([
+     #region Step 5 - Payment
+     public function getReadyForPayment()
+     {
+         $payment = Payment::updateOrCreate([
             'user_id' => auth()->id(),
             'booking_id' => $this->bookingId,
         ], [
@@ -215,48 +214,48 @@ use Stripe\SetupIntent;
             'payment_type' => $this->paymentType,
         ]);
 
-        $this->paymentAmount = $payment->amount;
+         $this->paymentAmount = $payment->amount;
 
-        if ($this->paymentMethod == 'stripe') {
-            if (! isset($this->paymentIntent)) {
-                $this->paymentIntent = auth()->user()->createSetupIntent();
-            }
+         if ($this->paymentMethod == 'stripe') {
+             if (! isset($this->paymentIntent)) {
+                 $this->paymentIntent = auth()->user()->createSetupIntent();
+             }
 
-            $this->dispatchBrowserEvent('getReadyForPayment', [
+             $this->dispatchBrowserEvent('getReadyForPayment', [
                 'clientSecret' => $this->paymentIntent->client_secret,
             ]);
-        }
-    }
+         }
+     }
 
-    public function updatedPaymentMethod()
-    {
-        $this->getReadyForPayment();
-    }
+     public function updatedPaymentMethod()
+     {
+         $this->getReadyForPayment();
+     }
 
-    public function updatedPaymentType()
-    {
-        $this->getReadyForPayment();
-    }
+     public function updatedPaymentType()
+     {
+         $this->getReadyForPayment();
+     }
 
-    public function cardSetupConfirmed(string $paymentMethod)
-    {
-        $user = auth()->user();
-        $user->createOrGetStripeCustomer();
-        $user->updateDefaultPaymentMethod($paymentMethod);
-        $user->invoiceFor('Booking(' . $this->paymentType . ') for Package #' . $this->package . ' Of Tour '
+     public function cardSetupConfirmed(string $paymentMethod)
+     {
+         $user = auth()->user();
+         $user->createOrGetStripeCustomer();
+         $user->updateDefaultPaymentMethod($paymentMethod);
+         $user->invoiceFor('Booking(' . $this->paymentType . ') for Package #' . $this->package . ' Of Tour '
             . $this->tours->find($this->tour)->first()->name, $this->paymentAmount * 100);
 
-        $this->reduceAvailability();
-        $this->currentStep++;
-    }
+         $this->reduceAvailability();
+         $this->currentStep++;
+     }
 
-    public function recordManualPayment()
-    {
-        $payment = Payment::whereBookingId($this->bookingId)->latest()->first();
-        $this->resetErrorBag();
+     public function recordManualPayment()
+     {
+         $payment = Payment::whereBookingId($this->bookingId)->latest()->first();
+         $this->resetErrorBag();
 
-        try {
-            app(UpdateManualPaymentAction::class)
+         try {
+             app(UpdateManualPaymentAction::class)
                 ->execute($payment, $this->manualType, $this->bookingId, auth()->user(), [
                     'amount' => $this->paymentAmount,
                     'payment_type' => $this->paymentType,
@@ -268,64 +267,64 @@ use Stripe\SetupIntent;
                     'card_cvc' => $this->cardCvc,
                     'paymentCashReceived' => $this->paymentCashReceived,
                 ]);
-            $this->reduceAvailability();
-            $this->currentStep++;
-        } catch (ValidationException $e) {
-            $this->setErrorBag($e->validator->errors());
-        }
-    }
+             $this->reduceAvailability();
+             $this->currentStep++;
+         } catch (ValidationException $e) {
+             $this->setErrorBag($e->validator->errors());
+         }
+     }
 
-    public function validateCard(string $field)
-    {
-        $this->validate([
+     public function validateCard(string $field)
+     {
+         $this->validate([
             $field => $this->validateCardRule[$field],
         ]);
 
-        if ($this->getErrorBag()->isEmpty() && $field == 'cardExpiry') {
-            $isBeforeNextMonth = Carbon::createFromFormat('m/y', $this->cardExpiry)->isBefore(Carbon::now());
-            if ($isBeforeNextMonth) {
-                $this->getErrorBag()->add('cardExpiry', __('The card is expired'));
-            }
-        }
-    }
+         if ($this->getErrorBag()->isEmpty() && $field == 'cardExpiry') {
+             $isBeforeNextMonth = Carbon::createFromFormat('m/y', $this->cardExpiry)->isBefore(Carbon::now());
+             if ($isBeforeNextMonth) {
+                 $this->getErrorBag()->add('cardExpiry', __('The card is expired'));
+             }
+         }
+     }
 
-    private function reduceAvailability(): void
-    {
-        collect($this->guests)
+     private function reduceAvailability(): void
+     {
+         collect($this->guests)
             ->each(function ($guest) {
                 $this->pricings->find($guest['pricing'])->decrement('available_capacity');
             });
-    }
-    #endregion
+     }
+     #endregion
 
-    #region Step 6 - Finish
-    private function finish()
-    {
-        session()->flash('success', __('Booking saved successfully'));
-        $this->redirectRoute('bookings.index');
-    }
-    #endregion
+     #region Step 6 - Finish
+     private function finish()
+     {
+         session()->flash('success', __('Booking saved successfully'));
+         $this->redirectRoute('bookings.index');
+     }
+     #endregion
 
-    #region Flow Control
-    public function previousStep()
-    {
-        if ($this->currentStep == 4) {
-            $this->updatePricings();
-        }
-        $this->currentStep--;
-        $this->resetErrorBag();
-    }
+     #region Flow Control
+     public function previousStep()
+     {
+         if ($this->currentStep == 4) {
+             $this->updatePricings();
+         }
+         $this->currentStep--;
+         $this->resetErrorBag();
+     }
 
-    public function nextStep()
-    {
-        match ($this->currentStep) {
-            1 => $this->updatedTour(),
+     public function nextStep()
+     {
+         match ($this->currentStep) {
+             1 => $this->updatedTour(),
             2 => $this->updatedPackage(),
             3 => $this->validateGuest(),
             4 => $this->saveBooking(),
             6 => $this->finish(),
             default => $this->currentStep++,
-        };
-    }
-    #endregion
-}
+         };
+     }
+     #endregion
+ }
