ab0182174670379dfed5a36ae7a3b841ec94c2de
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-08 13:04:15 +0800

    inprove seeder to more logic

--- database/factories/BookingGuestFactory.php
+++ database/factories/BookingGuestFactory.php
@@ -14,12 +14,13 @@ class BookingGuestFactory extends Factory
     protected $model = BookingGuest::class;
 
     #[ArrayShape(['name' => "string", 'package_pricing_id' => "int|mixed", 'booking_id' => "int|mixed", 'is_child' => "int"])]
-    public function definition()
+    public function definition(): array
     {
+        $booking = Booking::inRandomOrder()->first();
         return [
             'name' => $this->faker->name,
-            'package_pricing_id' => PackagePricing::inRandomOrder()->first()->id,
-            'booking_id' => Booking::inRandomOrder()->first()->id,
+            'package_pricing_id' => PackagePricing::wherePackageId($booking->package_id)->inRandomOrder()->first()->id,
+            'booking_id' => $booking->id,
             'is_child' => rand(0, 1)
         ];
     }
