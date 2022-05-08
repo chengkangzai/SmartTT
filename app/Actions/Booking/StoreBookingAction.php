ee232a85a3f3c80554f82a8151a81fde01fc2eea
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-08 13:03:29 +0800

    undo delete store booking action

---
+++ app/Actions/Booking/StoreBookingAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Actions\Booking;
+
+use App\Models\Booking;
+use App\Models\User;
+use function collect;
+
+class StoreBookingAction
+{
+    public function execute(User $user, array $data): Booking
+    {
+        $booking = Booking::create([
+            ...$data,
+            'user_id' => $user->id,
+            'discount' => 0, // TODO Coupon
+        ]);
+
+        collect($data['guests'])
+            ->each(function ($guest) use ($booking) {
+                $booking->guests()->create([
+                    'name' => $guest['name'],
+                    'price' => $guest['price'],
+                    'is_child' => $guest['is_child'],
+                    'package_pricing_id' => $guest['pricing'] ?: null,
+                ]);
+            });
+
+        return $booking->refresh();
+    }
+}
