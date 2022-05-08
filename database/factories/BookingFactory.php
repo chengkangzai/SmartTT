8c6f9ac53e7a2aa351e08a76caab6239842e4c77
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-08 23:14:29 +0800

    increase logic of booking seeder

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -3,10 +3,13 @@
 namespace Database\Factories;
 
 use App\Models\Booking;
+use App\Models\BookingGuest;
 use App\Models\Package;
+use App\Models\PackagePricing;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use JetBrains\PhpStorm\ArrayShape;
+use function rand;
 
 class BookingFactory extends Factory
 {
@@ -19,9 +22,28 @@ class BookingFactory extends Factory
             'user_id' => User::where('id', '>', 1)->inRandomOrder()->first()->id,
             'package_id' => Package::inRandomOrder()->first()->id,
             'total_price' => rand(100, 1000),
-            'discount' => rand(0, 100),
+            'discount' => 0,
             'adult' => rand(1, 10),
             'child' => rand(0, 3)
         ];
+    }
+
+    public function configure(): BookingFactory
+    {
+        return $this->afterCreating(function (Booking $booking) {
+            $guests = BookingGuest::factory(rand(2, 5))
+                ->afterCreating(function (BookingGuest $guest) use ($booking) {
+                    $guest->package_pricing_id = $guest->is_child
+                        ? null
+                        : PackagePricing::wherePackageId($booking->package_id)->inRandomOrder()->first()->id;
+                })
+                ->create([
+                    'booking_id' => $booking->id,
+                ]);
+
+            $booking->adult = $guests->where('is_child', false)->count();
+            $booking->child = $guests->where('is_child', true)->count();
+            $booking->save();
+        });
     }
 }
--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -7,38 +7,47 @@ use App\Actions\Booking\Invoice\GenerateReceiptAction;
 use App\Models\Booking;
 use App\Models\BookingGuest;
 use App\Models\Payment;
+use App\Models\Settings\BookingSetting;
 use Illuminate\Database\Seeder;
+use Log;
 use function app;
 use function collect;
+use function dd;
 
 class BookingSeeder extends Seeder
 {
     public function run()
     {
         Booking::factory(10)
-            ->afterCreating(function ($booking) {
-                $guests = BookingGuest::factory(rand(1, 3))->create([
-                    'booking_id' => $booking->id,
-                ]);
+            ->afterCreating(function (Booking $booking) {
+                $setting = app(BookingSetting::class);
+                $guests = collect($booking->guests)
+                    ->map(function (BookingGuest $guest) use ($setting) {
+                        return [
+                            'name' => $guest->name,
+                            'pricing' => $guest->package_pricing_id,
+                            'price' => $guest->packagePricing->price ?? $setting->charge_per_child,
+                        ];
+                    });
+
+                $booking->total_price = $guests->sum('price');
+                $booking->save();
+
                 $payments = Payment::factory()
                     ->count(rand(1, 2))
+                    ->afterCreating(function (Payment $payment) {
+                        if ($payment->status == Payment::STATUS_PAID && $payment->payment_type == Payment::TYPE_FULL) {
+                            $payment->amount = $payment->booking->total_price;
+                            $payment->save();
+                        }
+                    })
                     ->create([
                         'booking_id' => $booking->id,
                     ]);
 
-                $guests = collect($guests)
-                    ->map(function (BookingGuest $guest) {
-                        return [
-                            'name' => $guest->name,
-                            'pricing' => $guest->package_pricing_id,
-                            'price' => $guest->packagePricing->price,
-                        ];
-                    })
-                    ->toArray();
-
                 foreach ($payments as $payment) {
                     app(GenerateInvoiceAction::class)->execute($payment, [
-                        'guests' => $guests,
+                        'guests' => $guests->toArray(),
                     ]);
                     if ($payment->payment_type == 'full'
                         && ($payment->payment_method == 'cash'
@@ -47,7 +56,7 @@ class BookingSeeder extends Seeder
                         )
                     ) {
                         app(GenerateReceiptAction::class)->execute($payment, [
-                            'guests' => $guests,
+                            'guests' => $guests->toArray(),
                         ]);
                     }
                 }
