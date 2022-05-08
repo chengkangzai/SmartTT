ff1517b84fdb7780649fafc0ff661bab60e59fa5
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-08 23:13:59 +0800

    decrease the probability of booking guest and put null on package pricing on child

--- database/factories/BookingGuestFactory.php
+++ database/factories/BookingGuestFactory.php
@@ -16,12 +16,13 @@ class BookingGuestFactory extends Factory
     #[ArrayShape(['name' => "string", 'package_pricing_id' => "int|mixed", 'booking_id' => "int|mixed", 'is_child' => "int"])]
     public function definition(): array
     {
+        $isChild = rand(0, 100) > 90;
         $booking = Booking::inRandomOrder()->first();
         return [
             'name' => $this->faker->name,
-            'package_pricing_id' => PackagePricing::wherePackageId($booking->package_id)->inRandomOrder()->first()->id,
+            'package_pricing_id' => $isChild ? null : PackagePricing::wherePackageId($booking->package_id)->inRandomOrder()->first()->id,
             'booking_id' => $booking->id,
-            'is_child' => rand(0, 1)
+            'is_child' => $isChild
         ];
     }
 }
