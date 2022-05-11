de7693cef2fae5455bfb39a65a9c270d85dce6e0
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-11 13:57:09 +0800

    finish testing for invoice

---
+++ tests/Feature/Actions/Booking/Invoice/InvoiceActionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Actions\Booking\Invoice\InvoiceAction;
+use Database\Seeders\AirlineSeeder;
+use Database\Seeders\AirportSeeder;
+use Database\Seeders\BookingSeeder;
+use Database\Seeders\CountrySeeder;
+use Database\Seeders\FlightSeeder;
+use Database\Seeders\PackageSeeder;
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\TourSeeder;
+use Database\Seeders\UserRoleSeeder;
+use LaravelDaily\Invoices\Classes\InvoiceItem;
+use LaravelDaily\Invoices\Invoice;
+use function Pest\Laravel\seed;
+
+beforeEach(function () {
+    seed([
+        PermissionSeeder::class,
+        UserRoleSeeder::class,
+        CountrySeeder::class,
+        TourSeeder::class,
+        AirlineSeeder::class,
+        AirportSeeder::class,
+        FlightSeeder::class,
+        PackageSeeder::class,
+        BookingSeeder::class
+    ]);
+});
+
+it('should be able to generate invoice item for each guest', function () {
+    Mockery::mock('override', InvoiceAction::class)
+        ->shouldAllowMockingProtectedMethods()
+        ->shouldReceive('getItems')
+        ->andReturn([
+            (new InvoiceItem())
+        ]);
+});
