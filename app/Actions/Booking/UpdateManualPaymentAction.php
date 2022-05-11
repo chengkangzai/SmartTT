028153e6577c04db0cfa261a8a6630eeba529fbc
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-11 12:14:46 +0800

    improve action logic

--- app/Actions/Booking/UpdateManualPaymentAction.php
+++ app/Actions/Booking/UpdateManualPaymentAction.php
@@ -4,6 +4,7 @@ namespace App\Actions\Booking;
 
 use App\Models\Payment;
 use App\Models\User;
+use Exception;
 use Illuminate\Validation\ValidationException;
 
 class UpdateManualPaymentAction
@@ -23,55 +24,56 @@ class UpdateManualPaymentAction
      * @param array{} $data
      * @return Payment
      * @throws ValidationException
+     * @throws Exception
      */
     public function execute(Payment $payment, string $mode, int $bookingId, User $user, array $data): Payment
     {
         $this->bookingId = $bookingId;
         $this->user = $user;
         $this->mode = $mode;
-        if ($mode === 'card') {
+        if ($mode === Payment::METHOD_CARD) {
             $data = $this->validateCard($data);
-            $this->storeCard($payment, $data);
-        } else {
+            return $this->storeCard($payment, $user,$data);
+        }
+        if ($mode === Payment::METHOD_CASH) {
             $data = $this->validateCash($data);
-            $this->storeCash($payment, $data);
+            return $this->storeCash($payment, $user,$data);
         }
 
-        $payment->update([
-            'paid_at' => now(),
-            'status' => Payment::STATUS_PAID,
-        ]);
-
-        return $payment->refresh();
+        throw new Exception('Payment method not supported');
     }
 
-    private function storeCard(Payment $payment, array $data): void
+    private function storeCard(Payment $payment, User $user, array $data): Payment
     {
         $payment->update([
             ...$data,
             'status' => Payment::STATUS_PAID,
             'payment_method' => Payment::METHOD_CARD,
-            'user_id' => auth()->id(),
+            'user_id' => $user->id,
         ]);
 
         activity()
             ->performedOn($payment->booking)
             ->causedBy($this->user)
             ->log('Payment#' . $payment->id . '(Card) recorded for booking #' . $this->bookingId);
+
+        return $payment->refresh();
     }
 
-    private function storeCash(Payment $payment, array $data): void
+    private function storeCash(Payment $payment, User $user, array $data): Payment
     {
         $payment->update([
             ...$data,
             'status' => Payment::STATUS_PAID,
             'payment_method' => Payment::METHOD_CASH,
-            'user_id' => auth()->id(),
+            'user_id' => $user->id,
         ]);
 
         activity()
             ->performedOn($payment->booking)
             ->causedBy($this->user)
             ->log('Payment#' . $payment->id . '(Cash) recorded for booking #' . $this->bookingId);
+
+        return $payment->refresh();
     }
 }
