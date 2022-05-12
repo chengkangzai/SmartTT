e5c4c8801e4328de039b1a4a86b1a241fd5ba2eb
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-12 22:15:45 +0800

    remove debug

--- tests/Feature/Controllers/BookingControllerTest.php
+++ tests/Feature/Controllers/BookingControllerTest.php
@@ -64,7 +64,6 @@ it('should return add payment view', function () {
         ->create();
     $this
         ->get(route('bookings.addPayment', $booking))
-        ->dd()
         ->assertViewIs('smartTT.booking.add-payment')
         ->assertSeeLivewire('booking.add-payment-on-booking');
 });
