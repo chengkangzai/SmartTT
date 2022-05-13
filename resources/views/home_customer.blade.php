af4751c339cd0169648e3856488229b775956f19
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-13 18:27:06 +0800

    reformat code

--- resources/views/home_customer.blade.php
+++ resources/views/home_customer.blade.php
@@ -26,27 +26,33 @@
                             <tr>
                                 <td>{{ $booking->package->tour->name }}</td>
                                 <td>{{ $booking->package->depart_time }}</td>
-                                <td>{{ number_format($booking->total_price,2) }}</td>
-                                <td>{{ $booking->isFullPaid() ? __('Paid'): __('Pending') }}</td>
+                                <td>{{ number_format($booking->total_price, 2) }}</td>
+                                <td>{{ $booking->isFullPaid() ? __('Paid') : __('Pending') }}</td>
                                 <td>
-                                    @if($booking->isFullPaid())
-                                        <a href="{{ route('bookings.show', $booking) }}" class="btn btn-sm btn-outline-primary">{{ __('View') }}</a>
+                                    @if ($booking->isFullPaid())
+                                        <a href="{{ route('bookings.show', $booking) }}"
+                                            class="btn btn-sm btn-outline-primary">
+                                            {{ __('View') }}
+                                        </a>
                                     @else
-                                        <a href="{{ route('bookings.addPayment', $booking) }}" class="btn btn-sm btn-outline-primary">{{ __('Pay') }}</a>
+                                        <a href="{{ route('bookings.addPayment', $booking) }}"
+                                            class="btn btn-sm btn-outline-primary">
+                                            {{ __('Pay') }}
+                                        </a>
                                     @endif
                                 </td>
                             </tr>
                         @empty
                             <tr>
                                 <td colspan="4" class="text-center">
-                                    {{__('What are you waiting?')}}
+                                    {{ __('What are you waiting?') }}
                                     {!! __('<a href=":link" class="btn btn-sm btn-outline-primary">Make a Booking Here!</a>', ['link' => route('bookings.create')]) !!}
                                 </td>
                             </tr>
                         @endforelse
                     </tbody>
                 </table>
-                {{$bookings->links() }}
+                {{ $bookings->links() }}
             </div>
         </div>
 
@@ -64,24 +70,24 @@
                         <td>{{ __('Date') }}</td>
                     </thead>
                     <tbody>
-                    @forelse($payments as $payment)
-                        <tr>
-                            <td class="text-uppercase">{{ $payment->payment_method }}</td>
-                            <td class="text-uppercase">{{ $payment->payment_type }}</td>
-                            <td class="text-uppercase">{{ $payment->status }}</td>
-                            <td>{{ number_format($payment->amount,2) }}</td>
-                            <td>{{ $payment->created_at }}</td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4" class="text-center">
-                                {{__('No payment yet')}}
-                            </td>
-                        </tr>
-                    @endforelse
+                        @forelse($payments as $payment)
+                            <tr>
+                                <td class="text-uppercase">{{ $payment->payment_method }}</td>
+                                <td class="text-uppercase">{{ $payment->payment_type }}</td>
+                                <td class="text-uppercase">{{ $payment->status }}</td>
+                                <td>{{ number_format($payment->amount, 2) }}</td>
+                                <td>{{ $payment->created_at }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="4" class="text-center">
+                                    {{ __('No payment yet') }}
+                                </td>
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
-                {{$payments->links() }}
+                {{ $payments->links() }}
             </div>
         </div>
     </div>
