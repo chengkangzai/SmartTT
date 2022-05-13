d96045cae8e0057795fca4bc9a725b0ad896de25
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-13 12:29:26 +0800

    remove useless code

--- app/Jobs/StripeWebhooks/ChargeSucceededJob.php
+++ app/Jobs/StripeWebhooks/ChargeSucceededJob.php
@@ -39,17 +39,6 @@ class ChargeSucceededJob implements ShouldQueue
         if ($user) {
             $payment = Payment::where('user_id', $user->id)->whereNull('paid_at')->latest()->first();
             if ($payment) {
-                $charge = app(BookingSetting::class)->charge_per_child;
-                $guests = collect($payment->booking->guests)
-                    ->map(function (BookingGuest $guest) use ($charge) {
-                        return [
-                            'name' => $guest->name,
-                            'pricing' => $guest->package_pricing_id,
-                            'price' => $guest->packagePricing->price ?? $charge,
-                        ];
-                    })
-                    ->toArray();
-
                 $payment->update([
                     'paid_at' => now(),
                     'status' => Payment::STATUS_PAID,
