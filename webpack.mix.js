7ffb13f3c376097a688a9166d8b4ec6903f87533
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-13 16:46:59 +0800

    publish chart js by mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('node_modules/chart.js/dist/chart.js', 'public/js/chart.js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
