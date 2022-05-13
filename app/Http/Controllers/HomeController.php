181247b8c7fd887ba4832c8a230415138e61322f
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-13 18:28:59 +0800

    Actionize

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,54 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Booking;
-use App\Models\Package;
-use App\Models\Tour;
-use App\Models\User;
-use Spatie\Activitylog\Models\Activity;
+use App\Actions\Home\GetHomeDataForCustomer;
+use App\Actions\Home\GetHomeDataForStaff;
 
 class HomeController extends Controller
 {
     public function index()
     {
         if (!auth()->user()->roles->contains('name', 'Customer')) {
+            [$userCount, $userData, $bookingCount, $bookingData, $tourCount, $packageCount, $logs]
+                = app(GetHomeDataForStaff::class)->execute();
             return view('home', [
-                'userCount' => User::count(),
-                'userData' => [
-                    'label' => [__('Saturday'), __('Sunday'), __('Monday'), __('Tuesday'), __('Wednesday'), __('Thursday'), __('Friday')],
-                    'data' => [
-                        User::whereDay('created_at', '=', now()->subDays(6))->count(),
-                        User::whereDay('created_at', '=', now()->subDays(5))->count(),
-                        User::whereDay('created_at', '=', now()->subDays(4))->count(),
-                        User::whereDay('created_at', '=', now()->subDays(3))->count(),
-                        User::whereDay('created_at', '=', now()->subDays(2))->count(),
-                        User::whereDay('created_at', '=', now()->subDays(1))->count(),
-                        User::whereDay('created_at', '=', now())->count(),
-                    ]
-                ],
-                'bookingCount' => Booking::active()->count(),
-                'bookingData' => [
-                    'label' => [__('Saturday'), __('Sunday'), __('Monday'), __('Tuesday'), __('Wednesday'), __('Thursday'), __('Friday')],
-                    'data' => [
-                        Booking::whereDay('created_at', '=', now()->subDays(6))->count(),
-                        Booking::whereDay('created_at', '=', now()->subDays(5))->count(),
-                        Booking::whereDay('created_at', '=', now()->subDays(4))->count(),
-                        Booking::whereDay('created_at', '=', now()->subDays(3))->count(),
-                        Booking::whereDay('created_at', '=', now()->subDays(2))->count(),
-                        Booking::whereDay('created_at', '=', now()->subDays(1))->count(),
-                        Booking::whereDay('created_at', '=', now())->count(),
-                    ]
-                ],
-                //replace with income and revenue data
-                'tourCount' => Tour::active()->count(),
-                'packageCount' => Package::active()->count(),
-                'logs' => Activity::where('subject_type', Booking::class)->latest()->paginate(10),
+                'userCount' => $userCount,
+                'userData' => $userData,
+                'bookingCount' => $bookingCount,
+                'bookingData' => $bookingData,
+                'tourCount' => $tourCount,
+                'packageCount' => $packageCount,
+                'logs' => $logs,
             ]);
         }
 
+        [$bookings, $payments] = app(GetHomeDataForCustomer::class)->execute(auth()->user());
         return view('home_customer', [
-            'bookings' => auth()->user()->bookings()->paginate(10, ['*'], 'bookings'),
-            'payments' => auth()->user()->payments()->paginate(10, ['*'], 'payments'),
+            'bookings' => $bookings,
+            'payments' => $payments,
         ]);
 
     }
