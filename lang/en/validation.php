adbf5a125ba5742d3d586d1895f5b5ab0259938f
chengkangzai <chengkangzai@users.noreply.github.com>
2022-05-14 06:30:41 +0000

    Fix styling

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -1,6 +1,6 @@
 <?php
 
-return array (
+return [
   'accepted' => 'The :attribute must be accepted.',
   'accepted_if' => 'The :attribute must be accepted when :other is :value.',
   'active_url' => 'The :attribute is not a valid URL.',
@@ -12,23 +12,23 @@ return array (
   'array' => 'The :attribute must be an array.',
   'before' => 'The :attribute must be a date before :date.',
   'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
-  'between' => 
-  array (
+  'between' =>
+  [
     'array' => 'The :attribute must have between :min and :max items.',
     'file' => 'The :attribute must be between :min and :max kilobytes.',
     'numeric' => 'The :attribute must be between :min and :max.',
     'string' => 'The :attribute must be between :min and :max characters.',
-  ),
+  ],
   'boolean' => 'The :attribute field must be true or false.',
   'confirmed' => 'The :attribute confirmation does not match.',
   'current_password' => 'The password is incorrect.',
-  'custom' => 
-  array (
-    'attribute-name' => 
-    array (
+  'custom' =>
+  [
+    'attribute-name' =>
+    [
       'rule-name' => 'custom-message',
-    ),
-  ),
+    ],
+  ],
   'date' => 'The :attribute is not a valid date.',
   'date_equals' => 'The :attribute must be a date equal to :date.',
   'date_format' => 'The :attribute does not match the format :format.',
@@ -45,20 +45,20 @@ return array (
   'exists' => 'The selected :attribute is invalid.',
   'file' => 'The :attribute must be a file.',
   'filled' => 'The :attribute field must have a value.',
-  'gt' => 
-  array (
+  'gt' =>
+  [
     'array' => 'The :attribute must have more than :value items.',
     'file' => 'The :attribute must be greater than :value kilobytes.',
     'numeric' => 'The :attribute must be greater than :value.',
     'string' => 'The :attribute must be greater than :value characters.',
-  ),
-  'gte' => 
-  array (
+  ],
+  'gte' =>
+  [
     'array' => 'The :attribute must have :value items or more.',
     'file' => 'The :attribute must be greater than or equal to :value kilobytes.',
     'numeric' => 'The :attribute must be greater than or equal to :value.',
     'string' => 'The :attribute must be greater than or equal to :value characters.',
-  ),
+  ],
   'image' => 'The :attribute must be an image.',
   'in' => 'The selected :attribute is invalid.',
   'in_array' => 'The :attribute field does not exist in :other.',
@@ -67,37 +67,37 @@ return array (
   'ipv4' => 'The :attribute must be a valid IPv4 address.',
   'ipv6' => 'The :attribute must be a valid IPv6 address.',
   'json' => 'The :attribute must be a valid JSON string.',
-  'lt' => 
-  array (
+  'lt' =>
+  [
     'array' => 'The :attribute must have less than :value items.',
     'file' => 'The :attribute must be less than :value kilobytes.',
     'numeric' => 'The :attribute must be less than :value.',
     'string' => 'The :attribute must be less than :value characters.',
-  ),
-  'lte' => 
-  array (
+  ],
+  'lte' =>
+  [
     'array' => 'The :attribute must not have more than :value items.',
     'file' => 'The :attribute must be less than or equal to :value kilobytes.',
     'numeric' => 'The :attribute must be less than or equal to :value.',
     'string' => 'The :attribute must be less than or equal to :value characters.',
-  ),
+  ],
   'mac_address' => 'The :attribute must be a valid MAC address.',
-  'max' => 
-  array (
+  'max' =>
+  [
     'array' => 'The :attribute must not have more than :max items.',
     'file' => 'The :attribute must not be greater than :max kilobytes.',
     'numeric' => 'The :attribute must not be greater than :max.',
     'string' => 'The :attribute must not be greater than :max characters.',
-  ),
+  ],
   'mimes' => 'The :attribute must be a file of type: :values.',
   'mimetypes' => 'The :attribute must be a file of type: :values.',
-  'min' => 
-  array (
+  'min' =>
+  [
     'array' => 'The :attribute must have at least :min items.',
     'file' => 'The :attribute must be at least :min kilobytes.',
     'numeric' => 'The :attribute must be at least :min.',
     'string' => 'The :attribute must be at least :min characters.',
-  ),
+  ],
   'multiple_of' => 'The :attribute must be a multiple of :value.',
   'not_in' => 'The selected :attribute is invalid.',
   'not_regex' => 'The :attribute format is invalid.',
@@ -117,13 +117,13 @@ return array (
   'required_without' => 'The :attribute field is required when :values is not present.',
   'required_without_all' => 'The :attribute field is required when none of :values are present.',
   'same' => 'The :attribute and :other must match.',
-  'size' => 
-  array (
+  'size' =>
+  [
     'array' => 'The :attribute must contain :size items.',
     'file' => 'The :attribute must be :size kilobytes.',
     'numeric' => 'The :attribute must be :size.',
     'string' => 'The :attribute must be :size characters.',
-  ),
+  ],
   'starts_with' => 'The :attribute must start with one of the following: :values.',
   'string' => 'The :attribute must be a string.',
   'timezone' => 'The :attribute must be a valid timezone.',
@@ -131,4 +131,4 @@ return array (
   'uploaded' => 'The :attribute failed to upload.',
   'url' => 'The :attribute must be a valid URL.',
   'uuid' => 'The :attribute must be a valid UUID.',
-);
+];
