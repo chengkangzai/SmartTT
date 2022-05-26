ef315b2f1fefc9af19f645be611bb158d93aa892
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-26 20:44:27 +0800

    fix language file for audit

---
+++ lang/zh/constant.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Booking;
+use App\Models\Flight;
+use App\Models\Package;
+use App\Models\PackagePricing;
+use App\Models\Tour;
+use App\Models\TourDescription;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+return [
+    'activity' => [
+        'created' => '被创建',
+        'deleted' => '被删除',
+        'updated' => '被更新',
+    ],
+    'model' => [
+        Booking::class => __('Booking'),
+        Flight::class => __('Flight'),
+        Package::class => __('Package'),
+        PackagePricing::class => __('Package Pricing'),
+        Role::class => __('Role'),
+        User::class => __('User'),
+        Tour::class => __('Tour'),
+        TourDescription::class => __('Tour Description')
+    ]
+];
