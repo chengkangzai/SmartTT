c391e641cd4a1d88766c78d13cebf0291db26c78
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-26 21:34:09 +0800

    add register billing info step

---
+++ resources/views/livewire/booking/steps/register-billing-info-step.blade.php
@@ -0,0 +1,28 @@
+<div class="card">
+    <div class="card-header">
+        <h4 class="card-title">{{ __('Billing Information') }}</h4>
+    </div>
+    <div class="card-body row">
+        <div class="mb-3 col-12 col-md-6">
+            <label for="billing-name">{{ __('Full Name') }}</label>
+            <input type="text" class="form-control" id="billing-name" wire:model="billingName"
+                   wire:change.debounce="validateBilling('billingName')" placeholder="John Wick"/>
+        </div>
+        <div class="mb-3 col-12 col-md-6">
+            <label for="billing-phone">{{ __('Phone Number') }}</label>
+            <input type="text" class="form-control" id="billing-phone" wire:model="billingPhone"
+                   wire:change.debounce="validateBilling('billingPhone')" placeholder="60123456789"/>
+        </div>
+    </div>
+    <div class="card-footer">
+        <div class="float-end">
+            <button wire:click="previousStep" class="btn btn-primary mx-1" wire:loading.attr="disabled">
+                &larr; {{ __('Previous Step') }}
+            </button>
+            <button wire:click="nextStep" class="btn btn-primary mx-1" wire:loading.attr="disabled">
+                <span wire:loading class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                {{ __('Next Step') }} &rarr;
+            </button>
+        </div>
+    </div>
+</div>
