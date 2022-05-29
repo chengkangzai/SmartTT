312ff6fefb9480c70fcc72b7bcefaa34494af12d
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-29 10:43:28 +0800

    fix test

--- tests/Feature/Controllers/DashboardControllerTest.php
+++ tests/Feature/Controllers/DashboardControllerTest.php
@@ -2,15 +2,9 @@
 
 use App\Models\User;
 
-it('should return a view when its logged in', function () {
-    $this->actingAs(User::factory()->create())
-        ->get('/')
-        ->assertRedirect(route('home'));
-});
-
 it('should redirect when user is not logged in', function () {
-    $this->get('/')
-        ->assertRedirect('/login')
+    $this->get(route('home'))
+        ->assertRedirect(route('login'))
         ->assertStatus(302);
 });
 
