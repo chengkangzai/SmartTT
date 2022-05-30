4d8ed6b0dfc9aeb8bce4c1ad117bb8d1a0b2b7ea
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-31 00:31:04 +0800

    extract part of the view to Price card

---
+++ resources/views/livewire/front/index/tour/price-card.blade.php
@@ -0,0 +1,46 @@
+<div class="z-10 mx-auto flex flex-col rounded-xl border bg-white py-4 shadow-xl md:-mt-80">
+    <div class="mx-auto">
+        <h5 class="text-xl font-medium">{{ __('Price Start From') }}</h5>
+        <h1 class="text-3xl font-extrabold">
+            {{ $default_currency_symbol }}
+            {{ number_format($cheapestPackagePricing->price, 2) }}
+        </h1>
+    </div>
+    <div class="my-2 border"></div>
+    <div class="container mx-auto py-2 px-8">
+        <div class="flex w-full flex-col gap-1">
+            <label for="category" class="mx-auto">{{ __('Available Packages') }}</label>
+            <select name="category" id="category" class="rounded-lg" wire:model="packageId">
+                @foreach ($tour->activePackages as $category)
+                    <option value="{{ $category->id }}">
+                        {{ $category->depart_time->format('Y-m-d') }}
+                        ({{ $default_currency_symbol }}
+                        {{ $category->price }})
+                    </option>
+                @endforeach
+            </select>
+        </div>
+
+    </div>
+    <div class="flex w-full flex-col gap-1 px-4">
+        @foreach ($tour->packages->find($packageId)->pricings as $packagePrice)
+            <div class="flex w-full flex-row text-lg">
+                <div class="grow">
+                    {{ $packagePrice->name }}
+                    ({{ $packagePrice->available_capacity }}
+                    {{ __('Seat Left') }})
+                </div>
+                <div>
+                    ({{ $default_currency_symbol }}
+                    {{ number_format($packagePrice->price, 2) }})
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div class="mx-auto mt-4 py-4">
+        <button
+            class="block animate-bounce rounded-xl bg-green-500 px-8 py-2 font-bold ring ring-lime-200 hover:animate-none hover:bg-green-400">
+            {{ __('Book Now') }} &excl;
+        </button>
+    </div>
+</div>
