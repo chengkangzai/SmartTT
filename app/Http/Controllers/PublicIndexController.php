ee95849b1fbc97f8d3530e18a49cfb32955bd95e
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-05-30 13:07:34 +0800

    add activePackages query

--- app/Http/Controllers/PublicIndexController.php
+++ app/Http/Controllers/PublicIndexController.php
@@ -4,15 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Country;
 use App\Models\Tour;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class PublicIndexController extends Controller
 {
     public function index()
     {
         $categories = Tour::select('category')->distinct()->pluck('category');
-        $imageUrl = Tour::inRandomOrder()->first()->getFirstMedia('thumbnail')?->getUrl();
+        $imageUrl = Media::whereCollectionName('thumbnail')
+            ->whereModelType(Tour::class)
+            ->inRandomOrder()
+            ->first()
+            ->getUrl();
         $countries = Country::select(['id', 'name'])->has('tours')->get();
+        $tours = Tour::query()
+            ->with([
+                'media',
+                'activePackages:id,is_active,depart_time',
+                'activePackages.pricings:id,price,package_id',
+                'countries:id,name',
+            ])
+            ->active()
+            ->select(['id', 'name'])
+            ->limit(6)
+            ->get();
 
-        return view('front.index.index', compact('categories', 'countries', 'imageUrl'));
+        return view('front.index.index', compact('categories', 'countries', 'imageUrl', 'tours'));
     }
 }
