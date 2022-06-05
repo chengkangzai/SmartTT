e3a6d7709049e64d94d83c5c44620a440914dab6
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-06-06 00:27:16 +0800

    introduce botman

---
+++ config/botman/config.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'conversation_cache_time' => 40,
+    'user_cache_time' => 30,
+];
---
+++ config/botman/web.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'matchingData' => [
+        'driver' => 'web',
+    ],
+];
