e5e00911929a7d2fca1607ee6831cc2f29b9d97c
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-06-06 21:48:54 +0800

    fix test not working

--- tests/Feature/Controllers/BotManControllerTest.php
+++ tests/Feature/Controllers/BotManControllerTest.php
@@ -1,11 +1,9 @@
 <?php
 
 it('should successfully finish a post method call', function () {
-    $this->post(route('front.botman'))
-        ->assertSuccessful();
-});
-
-it('should successfully finish a get method call', function () {
-    $this->get(route('front.botman'))
+    $this->post(route('front.botman'), [
+        'driver' => 'web',
+        'message' => 'hi'
+    ])
         ->assertSuccessful();
 });
