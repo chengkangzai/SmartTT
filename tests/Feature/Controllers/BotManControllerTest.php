61b1f1f545f4d226f692c6b4ae3037be38f5dd55
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-06-06 22:17:24 +0800

    remove testing

--- tests/Feature/Controllers/BotManControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-it('should successfully finish a post method call', function () {
-    $this->post(route('front.botman'), [
-        'driver' => 'web',
-        'message' => 'hi'
-    ])
-        ->assertSuccessful();
-});
