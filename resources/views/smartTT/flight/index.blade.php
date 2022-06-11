40312113338b437a53f8de96f022348ec816afb6
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-06-11 21:09:22 +0800

    finish flight table

--- resources/views/smartTT/flight/index.blade.php
+++ resources/views/smartTT/flight/index.blade.php
@@ -24,55 +24,7 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="table-responsive">
-                <table id="indexTable" class="table table-bordered table-hover ">
-                    <thead>
-                        <tr>
-                            <th>{{ __('ID') }}</th>
-                            <th>{{ __('Depart Time') }}</th>
-                            <th>{{ __('Arrival Time') }}</th>
-                            <th>{{ __('Price') }}</th>
-                            <th>{{ __('Airline') }}</th>
-                            <th>{{ __('Action') }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($flights as $flight)
-                            <tr>
-                                <td>{{ $flight->id }}</td>
-                                <td>{{ $flight->departure_date->translatedFormat(config('app.date_format')) }}</td>
-                                <td>{{ $flight->arrival_date->translatedFormat(config('app.date_format')) }}</td>
-                                <td>{{ number_format($flight->price, 2) }}</td>
-                                <td>{{ $flight->airline->name }}
-                                    ({{ $flight->depart_airport->IATA }})
-                                    -> ({{ $flight->arrive_airport->IATA }})
-                                </td>
-                                <td>
-                                    @can('View Flight')
-                                        <a href="{{ route('flights.show', $flight) }}" class="btn btn-outline-info">
-                                            {{ __('Show') }}
-                                        </a>
-                                    @endcan
-                                    @can('Edit Flight')
-                                        <a href="{{ route('flights.edit', $flight) }}" class="btn btn-outline-primary">
-                                            {{ __('Edit') }}
-                                        </a>
-                                    @endcan
-                                    @can('Delete Flight')
-                                        <form action="{{ route('flights.destroy', $flight) }}" method="POST"
-                                            style="display: inline">
-                                            @method('DELETE')
-                                            @csrf
-                                            <input class="btn btn-outline-danger" type="submit" value="{{ __('Delete') }}" />
-                                        </form>
-                                    @endcan
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $flights->links() }}
-            </div>
+           <livewire:flight-table/>
         </div>
     </div>
 @endsection
