75bbe5b6ea830070bafc64f7cb5b13d8d885e469
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-06-11 20:48:52 +0800

    introduce package table

--- resources/views/smartTT/package/index.blade.php
+++ resources/views/smartTT/package/index.blade.php
@@ -25,62 +25,7 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="table-responsive">
-                <table id="indexTable" class="table table-bordered table-hover ">
-                    <thead>
-                        <tr>
-                            <th>{{ __('ID') }}</th>
-                            <th>{{ __('Departure') }}</th>
-                            <th>{{ __('Tour') }}</th>
-                            <th>{{ __('Airline') }}</th>
-                            <th>{{ __('Price') }} ({{ $setting->default_currency }})</th>
-                            <th>{{ __('Active') }}</th>
-                            <th>{{ __('Action') }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($packages as $package)
-                            <tr>
-                                <td>{{ $package->id }}</td>
-                                <td>{{ $package->depart_time->translatedFormat(config('app.date_format')) }}</td>
-                                <td>{{ $package->tour->name }}</td>
-                                <td>
-                                    <ol>
-                                        @foreach ($package->flight as $flight)
-                                            <li>{{ $flight->airline->name }}</li>
-                                        @endforeach
-                                    </ol>
-                                </td>
-                                <td>{{ __($package->price) }}</td>
-                                <td>
-                                    <x-active-inactive-badge :active="$package->is_active" />
-                                </td>
-                                <td>
-                                    @can('View Package')
-                                        <a href="{{ route('packages.show', $package) }}" class="btn btn-outline-info">
-                                            {{ __('Show') }}
-                                        </a>
-                                    @endcan
-                                    @can('Edit Package')
-                                        <a href="{{ route('packages.edit', $package) }}" class="btn btn-outline-primary">
-                                            {{ __('Edit') }}
-                                        </a>
-                                    @endcan
-                                    @can('Delete Package')
-                                        <form action="{{ route('packages.destroy', $package) }}" class="d-inline"
-                                            method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <input class="btn btn-outline-danger" type="submit" value="{{ __('Delete') }}" />
-                                        </form>
-                                    @endcan
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $packages->links() }}
-            </div>
+            <livewire:package-table/>
         </div>
     </div>
 @endsection
