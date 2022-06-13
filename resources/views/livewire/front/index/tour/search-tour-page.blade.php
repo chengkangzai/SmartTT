5d1914841328e941edbefcabdde0df60f77cfd73
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-06-13 08:41:46 +0800

    add powered by algolia

--- resources/views/livewire/front/index/tour/search-tour-page.blade.php
+++ resources/views/livewire/front/index/tour/search-tour-page.blade.php
@@ -74,6 +74,9 @@
                 <div class="flex flex-col">
                     <input value="{{ __('Search') }}" type="submit"
                         class="my-auto rounded bg-green-500 py-2 px-4 text-white hover:bg-green-600 hover:text-gray-50">
+                    <small>{{__('Powered by')}}
+                        <img class="h-4 d-inline" src="{{asset('icons/algolia.png')}}" alt="Powered By Algolia"/>
+                    </small>
                 </div>
             </div>
         </div>
