b8f2fa7c545d64f6d95c9677f27c5acf5e6df732
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-06-13 08:15:27 +0800

    undo deletion

--- app/Http/Livewire/Front/Index/Tour/SearchTourCard.php
+++ app/Http/Livewire/Front/Index/Tour/SearchTourCard.php
@@ -16,17 +16,32 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 class SearchTourCard extends Component
 {
     public string $latestDepartTime;
+    public Collection $categories;
+    public Collection $countries;
     public string $imageUrl;
+
+    public int $priceFrom;
+    public int $priceTo;
 
     public function mount()
     {
         $this->imageUrl = Media::whereCollectionName('thumbnail')
-                ->whereModelType(Tour::class)
-                ->inRandomOrder()
-                ?->first()
-                ?->getUrl() ?? '#';
+            ->whereModelType(Tour::class)
+            ->inRandomOrder()
+            ?->first()
+            ?->getUrl() ?? '#';
 
+        $this->countries = Country::select(['id', 'name'])->has('tours')->get();
         $this->latestDepartTime = Package::active()->latest('depart_time')->first()->depart_time->format('Y-m-d');
+
+        $sortByPrice = PackagePricing::with('activePackage:id')
+            ->select(['id', 'price'])
+            ->orderBy('price')
+            ->get()
+            ->pluck('price');
+
+        $this->priceFrom = $sortByPrice->first();
+        $this->priceTo = $sortByPrice->last();
     }
 
     public function render(): Factory|View|Application
