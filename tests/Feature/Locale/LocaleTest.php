6b6ce5b20357dde35b2b72661206b2db6d170e5d
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-07-07 15:39:40 +0800

    convert locale test to pest

---
+++ tests/Feature/Locale/LocaleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use function PHPUnit\Framework\assertArrayHasKey;
+use function PHPUnit\Framework\assertNotNull;
+
+
+it('should not have empty english key', function () {
+    $enLocale = json_decode(file_get_contents(lang_path() . '/en.json'), true);
+    foreach ($enLocale as $value) {
+        assertNotNull($value);
+    }
+});
+
+it('should have cn locale', function () {
+    $enLocale = json_decode(file_get_contents(lang_path() . '/en.json'), true);
+    $zhLocale = json_decode(file_get_contents(lang_path() . '/zh.json'), true);
+
+    foreach ($enLocale as $key => $value) {
+        assertArrayHasKey($key, $zhLocale);
+        assertNotNull($zhLocale[$key]);
+    }
+});
+
+it('should have ms locale', function () {
+    $enLocale = json_decode(file_get_contents(lang_path() . '/en.json'), true);
+    $msLocale = json_decode(file_get_contents(lang_path() . '/ms.json'), true);
+    foreach ($enLocale as $key => $value) {
+        assertArrayHasKey($key, $msLocale);
+        assertNotNull($msLocale[$key]);
+    }
+});
