ffc61ddd7896df917fff6744c608ffd30b08a25d
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-08-28 20:46:30 +0800

    allow all user to be able to access filament panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -13,7 +14,7 @@ use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use Notifiable;
     use HasRoles;
@@ -55,5 +56,10 @@ class User extends Authenticatable
     public function msOauth(): HasOne
     {
         return $this->hasOne(MicrosoftOAuth::class);
+    }
+
+    public function canAccessFilament(): bool
+    {
+        return true;
     }
 }
