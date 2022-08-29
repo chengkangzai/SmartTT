1a7740e38c224b9faa7fb3d04dcb77d40969ca52
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-08-29 17:27:24 +0800

    enable dark mode and hide footer in admin panel

--- config/filament.php
+++ config/filament.php
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'dark_mode' => false,
+    'dark_mode' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -198,7 +198,7 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
