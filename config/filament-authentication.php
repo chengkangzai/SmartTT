77e15ba58e6dc92617d6fee83aae4c3a70f190b7
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-08-30 17:40:25 +0800

    introduce phpsa/filament-authentication

---
+++ config/filament-authentication.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'models' => [
+        'User'       => \App\Models\User::class,
+        'Role'       => \Spatie\Permission\Models\Role::class,
+        'Permission' => \Spatie\Permission\Models\Permission::class,
+    ],
+    'resources'     => [
+        'UserResource'       => \Phpsa\FilamentAuthentication\Resources\UserResource::class,
+        'RoleResource'       => \Phpsa\FilamentAuthentication\Resources\RoleResource::class,
+        'PermissionResource' => \Phpsa\FilamentAuthentication\Resources\PermissionResource::class,
+    ],
+    'pages'         => [
+        'Profile' => \Phpsa\FilamentAuthentication\Pages\Profile::class
+    ],
+    'Widgets'       => [
+        'LatestUsers' => [
+            'enabled' => false,
+            'limit'   => 5,
+            'sort'    => 0
+        ],
+    ],
+    'preload_roles' => true,
+    'impersonate'   => [
+        'enabled'  => true,
+        'guard'    => 'web',
+        'redirect' => '/admin'
+    ]
+];
