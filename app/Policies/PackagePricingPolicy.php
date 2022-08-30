9edbcb63ef04e74b3e2b8ce933f7fa367cbced05
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-08-30 21:02:58 +0800

    fix typoe

--- app/Policies/PackagePricingPolicy.php
+++ app/Policies/PackagePricingPolicy.php
@@ -12,41 +12,41 @@ class PackagePricingPolicy
 
     public function viewAny(User $user): bool
     {
-        return $user->can('Access PackagePricing');
+        return $user->can('Access Package Pricing');
     }
 
     public function view(User $user, PackagePricing $packagePricing): bool
     {
-        return $user->can('View PackagePricing');
+        return $user->can('View Package Pricing');
     }
 
     public function create(User $user): bool
     {
-        return $user->can('Create PackagePricing');
+        return $user->can('Create Package Pricing');
     }
 
     public function update(User $user, PackagePricing $packagePricing): bool
     {
-        return $user->can('Edit PackagePricing');
+        return $user->can('Edit Package Pricing');
     }
 
     public function delete(User $user, PackagePricing $packagePricing): bool
     {
-        return $user->can('Delete PackagePricing');
+        return $user->can('Delete Package Pricing');
     }
 
     public function restore(User $user, PackagePricing $packagePricing): bool
     {
-        return $user->can('Delete PackagePricing');
+        return $user->can('Delete Package Pricing');
     }
 
     public function forceDelete(User $user, PackagePricing $packagePricing): bool
     {
-        return $user->can('Delete PackagePricing');
+        return $user->can('Delete Package Pricing');
     }
 
     public function audit(User $user, PackagePricing $packagePricing): bool
     {
-        return $user->can('Audit PackagePricing');
+        return $user->can('Audit Package Pricing');
     }
 }
--- app/Policies/TourDescriptionPolicy.php
+++ app/Policies/TourDescriptionPolicy.php
@@ -12,41 +12,41 @@ class TourDescriptionPolicy
 
     public function viewAny(User $user): bool
     {
-        return $user->can('Access TourDescription');
+        return $user->can('Access Tour Description');
     }
 
     public function view(User $user, TourDescription $tourDescription): bool
     {
-        return $user->can('View TourDescription');
+        return $user->can('View Tour Description');
     }
 
     public function create(User $user): bool
     {
-        return $user->can('Create TourDescription');
+        return $user->can('Create Tour Description');
     }
 
     public function update(User $user, TourDescription $tourDescription): bool
     {
-        return $user->can('Edit TourDescription');
+        return $user->can('Edit Tour Description');
     }
 
     public function delete(User $user, TourDescription $tourDescription): bool
     {
-        return $user->can('Delete TourDescription');
+        return $user->can('Delete Tour Description');
     }
 
     public function restore(User $user, TourDescription $tourDescription): bool
     {
-        return $user->can('Delete TourDescription');
+        return $user->can('Delete Tour Description');
     }
 
     public function forceDelete(User $user, TourDescription $tourDescription): bool
     {
-        return $user->can('Delete TourDescription');
+        return $user->can('Delete Tour Description');
     }
 
     public function audit(User $user, TourDescription $tourDescription): bool
     {
-        return $user->can('Audit TourDescription');
+        return $user->can('Audit Tour Description');
     }
 }
