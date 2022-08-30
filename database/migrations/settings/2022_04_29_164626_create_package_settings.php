a645d2cafbc9dcabfc0205bf2d0413420f5fb9e7
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-08-30 21:47:27 +0800

    add default pricing to package setting

--- database/migrations/settings/2022_04_29_164626_create_package_settings.php
+++ database/migrations/settings/2022_04_29_164626_create_package_settings.php
@@ -7,5 +7,22 @@ class CreatePackageSettings extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('package.default_status', true);
+        $this->migrator->add('package.default_pricing', [
+            [
+                'status' => true,
+                'name' => 'Early Bird',
+                'capacity' => 5,
+            ],
+            [
+                'status' => true,
+                'name' => 'Regular',
+                'capacity' => 25,
+            ],
+            [
+                'status' => true,
+                'name' => 'Late Bird',
+                'capacity' => 10,
+            ]
+        ]);
     }
 }
