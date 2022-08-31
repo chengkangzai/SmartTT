3f7b9cb4bb4d50197c5d9dcd99607d1edba8c7dd
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-08-31 11:28:33 +0800

    publish vendor translation

---
+++ lang/vendor/filament-authentication/zh/filament-authentication.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    'section.group'                 => '验证',
+    'section.users'                 => '用户',
+    'section.user'                  => '用户',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Fields
+        |--------------------------------------------------------------------------
+    */
+    'field.id'                      => 'ID',
+    'field.user.name'               => '名字',
+    'field.user.email'              => '电子邮件',
+    'field.user.roles'              => '用户组',
+    'field.user.verified_at'        => '认证于',
+    'field.user.created_at'         => '加入于',
+    'field.user.password'           => '密码',
+    'field.user.confirm_password'   => '确认密码',
+
+    'field.guard_name'              => '守卫名称',
+    'field.name'                    => '名字',
+    'field.permissions'             => '权限',
+    'field.roles'                   => '用户组',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Labels
+        |--------------------------------------------------------------------------
+    */
+
+    'section.permission'            => '权限',
+    'section.permissions'           => '权限',
+    'section.role'                  => '用户组',
+    'section.roles'                 => '用户组',
+    'section.roles_and_permissions' => '用户组和权限',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Filters
+        |--------------------------------------------------------------------------
+    */
+    'filter.verified'               => '电子邮件已验证',
+
+    'button.impersonate'            => "模拟用户",
+    'text.impersonating'            => '正在模拟用户: ',
+    'text.impersonating.end'        => ' - 结束模拟 ',
+];
