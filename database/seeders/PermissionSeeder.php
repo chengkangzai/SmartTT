1e24e4ab02f742623c6142af765b2c5f357302e6
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-08-31 11:28:47 +0800

    add Airline for permission

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -9,7 +9,7 @@ class PermissionSeeder extends Seeder
 {
     public function run()
     {
-        $modules = ['Tour', 'Tour Description', 'Package', 'Package Pricing', 'Flight', 'Role', 'Booking', 'Payment', 'User', 'Permission', 'Airport'];
+        $modules = ['Tour', 'Tour Description', 'Package', 'Package Pricing', 'Flight', 'Role', 'Booking', 'Payment', 'User', 'Permission', 'Airport', 'Airline'];
         $operation = ['Access', 'Create', 'View', 'Edit', 'Delete', 'Audit'];
         $temp = collect([]);
         foreach ($modules as $module) {
