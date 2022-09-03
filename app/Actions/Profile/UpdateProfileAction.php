75b116ded9f78bea37a7ef85d2d78ceea359b816
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-09-03 20:53:38 +0800

    bring back profile action

---
+++ app/Actions/Profile/UpdateProfileAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions\Profile;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UpdateProfileAction
+{
+    public function execute(array $data, User $user): User
+    {
+        $data = \Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|string|max:255|unique:users,email,' . $user->id,
+            'password' => 'nullable|string|confirmed|min:8',
+        ], customAttributes: [
+            'name' => __('Name'),
+            'email' => __('Email'),
+            'password' => __('Password'),
+        ])->validate();
+
+        if (isset($data['password'])) {
+            $user->update(['password' => Hash::make($data['password'])]);
+        }
+
+        $user->update([
+            ...$data,
+        ]);
+
+        return $user->refresh();
+    }
+}
