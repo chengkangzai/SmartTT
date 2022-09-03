098fd2e564a495731783b881bbd64405c88d5adf
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-09-03 20:56:14 +0800

    use request() to get package id

--- resources/views/filament/resources/booking-resource/pages/booking-wizard.blade.php
+++ resources/views/filament/resources/booking-resource/pages/booking-wizard.blade.php
@@ -1,5 +1,5 @@
 <x-filament::page>
     <link href="{{ asset('booking_css/booking_css.css') }}" rel="stylesheet">
-    <livewire:booking.create-booking-wizard :package-id="1"/>
+    <livewire:booking.create-booking-wizard :package-id="request('package_id')"/>
     @include('smartTT.partials.initialStripeScript')
 </x-filament::page>
