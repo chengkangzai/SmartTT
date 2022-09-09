bf0d3e389a85654362734faf4c0a0ba82ba9524e
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-09-10 03:00:21 +0800

    fix invoice have no translation

---
+++ lang/vendor/invoices/ms/invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Invois',
+    'serial'                 => 'No Siri',
+    'date'                   => 'Tarikh invois',
+    'seller'                 => 'Penjual',
+    'buyer'                  => 'Pembeli',
+    'address'                => 'Alamat',
+    'code'                   => 'Kod',
+    'vat'                    => 'Kod VAT',
+    'phone'                  => 'Telefon',
+    'description'            => 'Penerangan',
+    'units'                  => 'Unit',
+    'quantity'               => 'Jumlah',
+    'price'                  => 'Harga',
+    'discount'               => 'Diskaun',
+    'tax'                    => 'Cukai',
+    'sub_total'              => 'Jumlah kecil',
+    'total_discount'         => 'Jumlah diskaun',
+    'taxable_amount'         => 'Jumlah bercukai',
+    'total_taxes'            => 'Jumlah cukai',
+    'tax_rate'               => 'Kadar cukai',
+    'total_amount'           => 'Jumlah keseluruhan',
+    'pay_until'              => 'Sila bayar sebelum',
+    'amount_in_words'        => 'Jumlah dalam perkataan',
+    'amount_in_words_format' => '%s %s dan %s %s',
+    'notes'                  => 'Nota',
+    'shipping'               => 'Penghantaran',
+    'paid'                   => 'Dibayar',
+    'due'                    => 'Kerana',
+];
