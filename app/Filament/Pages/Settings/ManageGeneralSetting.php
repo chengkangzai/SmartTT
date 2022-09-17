86250d04fe9164d87bfaa4cc94d075d37f732df5
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-09-17 21:35:28 +0800

    convert section to tab

--- app/Filament/Pages/Settings/ManageGeneralSetting.php
+++ app/Filament/Pages/Settings/ManageGeneralSetting.php
@@ -9,6 +9,7 @@ use Closure;
 use DateTimeZone;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\SettingsPage;
@@ -64,79 +65,84 @@ class ManageGeneralSetting extends SettingsPage
             ->toArray();
 
         return [
-            Section::make(__('Site Settings'))
-                ->schema([
-                    TextInput::make('site_name')
-                        ->label(__('Site Name'))
-                        ->columnSpan(2)
-                        ->disabled(auth()->user()->cannot('Edit Setting'))
-                        ->required(),
-                    Select::make('default_language')
-                        ->label(__('Default Language'))
-                        ->options($languages)
-                        ->disabled(auth()->user()->cannot('Edit Setting'))
-                        ->required(),
-                    Select::make('default_timezone')
-                        ->searchable()
-                        ->label(__('Default Timezone'))
-                        ->afterStateHydrated(function (Closure $set) {
-                            $timezone = app(GeneralSetting::class)->default_timezone->getName();
-                            $set('default_timezone', $timezone);
-                        })
-                        ->options($timezones)
-                        ->disabled(auth()->user()->cannot('Edit Setting'))
-                        ->required(),
-                    Select::make('default_currency')
-                        ->label(__('Default Currency'))
-                        ->searchable()
-                        ->reactive()
-                        ->afterStateUpdated(function (Closure $get, Closure $set) {
-                            $currency = $get('default_currency');
-                            $symbol = config('money.'.$currency);
-                            $set('default_currency_symbol', $symbol['symbol']);
-                        })
-                        ->options($currencies)
-                        ->disabled(auth()->user()->cannot('Edit Setting'))
-                        ->required(),
-                    TextInput::make('default_currency_symbol')
-                        ->disabled(),
-                    Select::make('default_country')
-                        ->label(__('Default Country'))
-                        ->searchable()
-                        ->options($countries)
-                        ->disabled(auth()->user()->cannot('Edit Setting'))
-                        ->required(),
-                ])->columns(2),
-            Section::make(__('Company Settings'))
-                ->schema([
-                    TextInput::make('company_name')
-                        ->label(__('Company Name'))
-                        ->required()
-                        ->maxLength(255)
-                        ->disabled(auth()->user()->cannot('Edit Setting')),
-                    TextInput::make('company_phone')
-                        ->label(__('Company Phone'))
-                        ->required()
-                        ->maxLength(255)
-                        ->disabled(auth()->user()->cannot('Edit Setting')),
-                    TextInput::make('company_email')
-                        ->label(__('Company Email'))
-                        ->required()
-                        ->maxLength(255)
-                        ->disabled(auth()->user()->cannot('Edit Setting')),
-                    TextInput::make('business_registration_no')
-                        ->label(__('Business Registration Number'))
-                        ->required()
-                        ->maxLength(255)
-                        ->disabled(auth()->user()->cannot('Edit Setting')),
-                    Textarea::make('company_address')
-                        ->columnSpan(2)
-                        ->label(__('Company Address'))
-                        ->required()
-                        ->maxLength(255)
-                        ->disabled(auth()->user()->cannot('Edit Setting'))
-                        ->rows(4),
-                ])->columns(2),
+            Tabs::make('Heading')
+                ->tabs([
+                    Tabs\Tab::make(__('Site Settings'))
+                        ->schema([
+                            TextInput::make('site_name')
+                                ->label(__('Site Name'))
+                                ->columnSpan(2)
+                                ->disabled(auth()->user()->cannot('Edit Setting'))
+                                ->required(),
+                            Select::make('default_language')
+                                ->label(__('Default Language'))
+                                ->options($languages)
+                                ->disabled(auth()->user()->cannot('Edit Setting'))
+                                ->required(),
+                            Select::make('default_timezone')
+                                ->searchable()
+                                ->label(__('Default Timezone'))
+                                ->afterStateHydrated(function (Closure $set) {
+                                    $timezone = app(GeneralSetting::class)->default_timezone->getName();
+                                    $set('default_timezone', $timezone);
+                                })
+                                ->options($timezones)
+                                ->disabled(auth()->user()->cannot('Edit Setting'))
+                                ->required(),
+                            Select::make('default_currency')
+                                ->label(__('Default Currency'))
+                                ->searchable()
+                                ->reactive()
+                                ->afterStateUpdated(function (Closure $get, Closure $set) {
+                                    $currency = $get('default_currency');
+                                    $symbol = config('money.' . $currency);
+                                    $set('default_currency_symbol', $symbol['symbol']);
+                                })
+                                ->options($currencies)
+                                ->disabled(auth()->user()->cannot('Edit Setting'))
+                                ->required(),
+                            TextInput::make('default_currency_symbol')
+                                ->disabled(),
+                            Select::make('default_country')
+                                ->label(__('Default Country'))
+                                ->searchable()
+                                ->options($countries)
+                                ->disabled(auth()->user()->cannot('Edit Setting'))
+                                ->required(),
+                        ]),
+                    Tabs\Tab::make(__('Company Settings'))
+                        ->schema([
+                            TextInput::make('company_name')
+                                ->label(__('Company Name'))
+                                ->required()
+                                ->maxLength(255)
+                                ->disabled(auth()->user()->cannot('Edit Setting')),
+                            TextInput::make('company_phone')
+                                ->label(__('Company Phone'))
+                                ->required()
+                                ->maxLength(255)
+                                ->disabled(auth()->user()->cannot('Edit Setting')),
+                            TextInput::make('company_email')
+                                ->label(__('Company Email'))
+                                ->required()
+                                ->maxLength(255)
+                                ->disabled(auth()->user()->cannot('Edit Setting')),
+                            TextInput::make('business_registration_no')
+                                ->label(__('Business Registration Number'))
+                                ->required()
+                                ->maxLength(255)
+                                ->disabled(auth()->user()->cannot('Edit Setting')),
+                            Textarea::make('company_address')
+                                ->columnSpan(2)
+                                ->label(__('Company Address'))
+                                ->required()
+                                ->maxLength(255)
+                                ->disabled(auth()->user()->cannot('Edit Setting'))
+                                ->rows(4),
+                        ]),
+                ])
+                ->columns(2)
+                ->columnSpan(2)
         ];
     }
 
