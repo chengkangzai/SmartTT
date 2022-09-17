41f03377acf66c6476d88b0e9baddb6acff9c4a7
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-09-17 21:35:50 +0800

    remove space between

--- app/Models/Settings/GeneralSetting.php
+++ app/Models/Settings/GeneralSetting.php
@@ -8,27 +8,17 @@ use Spatie\LaravelSettings\Settings;
 class GeneralSetting extends Settings
 {
     public string $site_name;
-
     public string $default_language;
-
     public array $supported_language;
-
     public DateTimeZone $default_timezone;
-
     public string $default_currency;
-
     public string $default_currency_symbol;
-
     public string $default_country;
 
     public string $company_name;
-
     public string $company_address;
-
     public string $company_phone;
-
     public string $company_email;
-
     public string $business_registration_no;
 
     public static function group(): string
