29719dbcdc36a47929786ee70a3226fb27599cc7
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-09-17 11:50:54 +0800

    add user relationship on feedback model

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -19,7 +20,6 @@ class Feedback extends Model implements HasMedia
 
     protected $fillable=[
         'content',
-        'email',
         'name',
         'is_listed',
         'user_id'
@@ -30,4 +30,8 @@ class Feedback extends Model implements HasMedia
         return LogOptions::defaults()->logFillable()->logOnlyDirty();
     }
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
