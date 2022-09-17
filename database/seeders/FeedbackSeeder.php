bcb5059baa4b676b4b731f5cddf39f1559dc3e57
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-09-17 11:51:05 +0800

    seed feedback on seeder

--- database/seeders/FeedbackSeeder.php
+++ database/seeders/FeedbackSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feedback;
 use Illuminate\Database\Seeder;
 
 class FeedbackSeeder extends Seeder
 {
     public function run()
     {
-
+        Feedback::factory()->count(10)->create();
     }
 }
