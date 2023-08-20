90e3cdc64dd79232eb834515f4b44fe959402733
Ching Cheng Kang <pycck@hotmail.com>
2023-08-20 20:07:25 +0800

    improve UI

--- tests/Feature/Controllers/LocaleControllerTest.php
+++ tests/Feature/Controllers/LocaleControllerTest.php
@@ -1,20 +1,30 @@
 <?php
 
 use App\Models\User;
+use Database\Seeders\TourSeeder;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+use function Pest\Laravel\seed;
 
 it('should use the default locale', function () {
-    $this->actingAs(User::factory()->create())
-        ->get('/')
+    get('/')
         ->assertSee('Tours')
-        ->assertSee('Featured Tour');
+        ->assertDontSeeText('Featured Tour');
+});
+
+it('should use the default locale and see Featured Tour', function () {
+    seed(TourSeeder::class);
+
+    get('/')
+        ->assertSee('Tours')
+        ->assertDontSeeText('Featured Tour');
 });
 
 it('should change the locale', function () {
     $locales = ['ms', 'zh'];
     foreach ($locales as $locale) {
-        $this->actingAs(User::factory()->create())
-            ->get('/', ['locale' => $locale])
+        get('/', ['locale' => $locale])
             ->assertSee(__('Tours'))
-            ->assertSee(__('Featured Tour'));
+            ->assertDontSeeText(__('Featured Tour'));
     }
 });
