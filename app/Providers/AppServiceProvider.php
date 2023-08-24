a88c18663cd705577e6a5563e887071d9c8bdc2b
Ching Cheng Kang <pycck@hotmail.com>
2023-08-24 20:29:08 +0800

    bug fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,8 +29,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if (App::isProduction()) {
-            URL::forceScheme('https');
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
         }
 
         Builder::macro('count', function () {
