0ea5a0f344eea6d5ef0903071535f1402639d427
Ching Cheng Kang <pycck@hotmail.com>
2023-09-07 11:49:43 +0800

    update ui

--- resources/views/livewire/make-feedback.blade.php
+++ resources/views/livewire/make-feedback.blade.php
@@ -1,4 +1,4 @@
-<div class="container mx-auto my-6">
+<div class="mx-auto max-w-screen-sm my-6">
     <div class="bg-white rounded px-8 pt-6 pb-8 mb-4">
         <h2 class="text-xl font-bold mb-4">
             {{__('Send Us Your Feedback')}}
