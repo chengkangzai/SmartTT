639454cca735459bbb48de0fcfb282f7321f6785
Ching Cheng Kang <pycck@hotmail.com>
2023-09-26 23:37:10 +0800

    introduce seo package & upgrade dependencies

--- config/seo.php
+++ config/seo.php
@@ -59,7 +59,7 @@ return [
      *
      * You can use the following filetypes: ico, png, gif, jpeg, svg.
      */
-    'favicon' => null,
+    'favicon' => '/favicon.ico',
 
     'title' => [
         /**
